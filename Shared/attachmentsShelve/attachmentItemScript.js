31a74193fe391d6b2139480f5f27133b3f9a7c4e
Thijs Wenker <me@thoys.nl>
2017-08-24 00:48:20 +0200

    attachment items can now rezzed by server script (allows for skipping rez-permissions)

--- Shared/attachmentsShelve/attachmentItemScript.js
+++ Shared/attachmentsShelve/attachmentItemScript.js
@@ -61,8 +61,15 @@
     var _isGrabbing = false;
     var _attachmentData = null;
     var _leftRightAttachToggle = false;
+    var _listeningChannel = null;
+    var _virtualHoldController = null;
+    
+    var WANT_DEBUG = true;
 
+    var ATTACHMENT_CHANNEL_PREFIX = 'attachmentItem-';
 
+    var TOTAL_HOLD_LIFETIME = 60; // seconds
+    
     var DEFAULT_SNAP_DISTANCE = 0.3;
     var HAND_LEFT = 0;
     var HAND_RIGHT = 1;
@@ -72,13 +79,136 @@
     var ATTACH_SOUND = SoundCache.getSound(Script.resolvePath('sound/attach_sound_1.wav'));
     var DETACH_SOUND = SoundCache.getSound(Script.resolvePath('sound/detach.wav'));
 
+    var debugPrint = function(message) {};
+    if (WANT_DEBUG) {
+        debugPrint = function(message) {
+            print(message);
+        }
+    }
+
+    var getEntityProperty = function(entityID, property) {
+        return Entities.getEntityProperties(entityID, property)[property];
+    };
+
+    var _this;
+    function VirtualHoldController(hand, holdableEntity, localPosition, localRotation) {
+        _this = this;
+        _this.hand = hand;
+        _this.holdableEntity = holdableEntity;
+        _this.localPosition = localPosition;
+        _this.localRotation = localRotation;
+        _this.controllerWasReleased = false;
+        _this.cleanedUp = false;
+        _this.init();
+    }
+
+    VirtualHoldController.prototype = {
+        hand: null,
+        holdableEntity: null,
+        holdAction: null,
+        controllerWasReleased: null,
+        mappingName: null,
+        mapping: null,
+        cleanedUp: null,
+        onRelease: null,
+        init: function() {
+            Script.update.connect(_this.update);
+            Messages.sendMessage('Hifi-Hand-Disabler', _this.hand);
+
+            _this.mappingName = 'VirtualHoldController_' + _this.hand + '_' + _this.holdableEntity;
+
+            _this.mapping = Controller.newMapping(_this.mappingName);
+            var SMOOTH_TRIGGER_RELEASE_THRESHOLD = 0.3;
+            var GRIP_BUTTON_ACTIVE_THRESHOLD = 0.5;
+            var smoothTriggerCheck = function(value) {
+                if (value < SMOOTH_TRIGGER_RELEASE_THRESHOLD) {
+                    _this.release();
+                }
+            };
+            var gripButtonCheck = function(value) {
+                if (value > GRIP_BUTTON_ACTIVE_THRESHOLD) {
+                    _this.release();
+                }
+            }
+
+            var NEAR_GRABBING_ACTION_TIMEFRAME = 0.05;
+            var NEAR_GRABBING_KINEMATIC = true;
+            var NEAR_GRABBING_IGNORE_IK = false;
+            _this.holdAction = Entities.addAction('hold', _this.holdableEntity, {
+                hand: _this.hand,
+                timeScale: NEAR_GRABBING_ACTION_TIMEFRAME,
+                relativePosition: _this.localPosition,
+                relativeRotation: _this.localRotation,
+                ttl: TOTAL_HOLD_LIFETIME,
+                kinematic: NEAR_GRABBING_KINEMATIC,
+                kinematicSetVelocity: true,
+                ignoreIK: NEAR_GRABBING_IGNORE_IK
+            });
+
+            if (_this.hand === 'left') {
+                _this.mapping.from([Controller.Standard.LT]).peek().to(smoothTriggerCheck);
+                _this.mapping.from([Controller.Standard.LB]).peek().to(gripButtonCheck);
+                _this.mapping.from([Controller.Standard.LeftGrip]).peek().to(gripButtonCheck);
+            } else {
+                _this.mapping.from([Controller.Standard.RT]).peek().to(smoothTriggerCheck);
+                _this.mapping.from([Controller.Standard.RB]).peek().to(gripButtonCheck);
+                _this.mapping.from([Controller.Standard.RightGrip]).peek().to(gripButtonCheck);
+            }
+            _this.mapping.enable();
+
+            Entities.callEntityMethod(_this.holdableEntity, 'startNearGrab', [_this.hand, MyAvatar.sessionUUID]);
+
+        },
+        update: function(deltaTime) {
+            // check if entity still exists
+            if (Object.keys(Entities.getEntityProperties(_this.holdableEntity)).length === 0) {
+                debugPrint('Could not find holdableEntity. Cleaning up!');
+                _this.cleanup();
+                return;
+            }
+            // check if controller released
+            if (_this.controllerWasReleased) {
+                debugPrint('Controller was already released. Cleaning up!');
+                _this.cleanup();
+                return;
+            }
+            Entities.callEntityMethod(_this.holdableEntity, 'continueNearGrab', [_this.hand, MyAvatar.sessionUUID]);
+        },
+        release: function() {
+            if (_this.controllerWasReleased) {
+                debugPrint('Controller was already released!');
+                return;
+            }
+            _this.controllerWasReleased = true;
+            Entities.callEntityMethod(_this.holdableEntity, 'releaseGrab', [_this.hand, MyAvatar.sessionUUID]);
+
+            Entities.deleteAction(_this.holdableEntity, _this.holdAction);
+
+            if (_this.onRelease !== null) {
+                _this.onRelease.call(_this);
+            }
+
+            debugPrint('We don\'t need a virtual controller after release. Cleaning up!');
+            _this.cleanup();
+        },
+        cleanup: function() {
+            if (_this.cleanedUp) {
+                return;
+            }
+            _this.mapping.disable();
+            Messages.sendMessage('Hifi-Hand-Disabler', 'none');
+            Script.update.disconnect(_this.update);
+            _this.cleanedUp = true;
+        }
+    };
+
 
     var getUserData = function() {
         try {
-            return JSON.parse(Entities.getEntityProperties(_entityID, 'userData').userData);
+            return JSON.parse(getEntityProperty(_entityID, 'userData'));
         } catch (e) {
             // e
-            print('Could not retrieve valid userData');
+            debugPrint('Could not retrieve valid userData');
         }
         return null;
     };
@@ -91,10 +221,71 @@
         return userDataObject.Attachment;
     };
 
+    var getScriptTimestamp = function() {
+        return getEntityProperty(_entityID, 'scriptTimestamp');
+    }
+
+    var messageHandler = function(channel, message, sender, localOnly) {
+        if (channel !== _listeningChannel || localOnly) {
+            return;
+        }
+
+        try {
+            var data = JSON.parse(message);
+            print('receivedData: ' + JSON.stringify(data));
+            if (data.sessionUUID !== MyAvatar.sessionUUID) {
+                print('Session UUID does not match');
+                return;
+            }
+            if (data.action === 'attach') {
+                var attempts = 0;
+                var MAX_ATTEMPTS = 10;
+                var attachAttemptInterval = null;
+                var attachOnEntityFound = function() {
+                    if (Object.keys(Entities.getEntityProperties(data.entityID, 'position')).length === 0) {
+                        attempts++;
+                        if (attempts >= MAX_ATTEMPTS) {
+                            Script.clearInterval(attachAttemptInterval);
+                        }
+                        return;
+                    }
+                    Script.clearInterval(attachAttemptInterval);
+                    if (_virtualHoldController !== null) {
+                        _virtualHoldController.cleanup();
+                        _virtualHoldController = null;
+                    }
+
+                    _virtualHoldController = new VirtualHoldController(data.hand, data.entityID, data.localPosition, data.localRotation);
+                    _virtualHoldController.onRelease = function() {
+                        Messages.sendMessage(_listeningChannel, JSON.stringify({
+                            action: 'release',
+                            entityID: data.entityID
+                        }));
+                    };
+                };
+                attachAttemptInterval = Script.setInterval(attachOnEntityFound, 100);
+            }
+        } catch (e) {
+
+        }
+    };
+
     this.preload = function(entityID) {
         _entityID = entityID;
-        print('loaded ' + entityID);
+        debugPrint('loaded ' + entityID);
+        _listeningChannel = ATTACHMENT_CHANNEL_PREFIX + entityID;
+        Messages.subscribe(_listeningChannel);
+
+        Messages.messageReceived.connect(messageHandler);
     };
+
+    this.unload = function() {
+        Messages.messageReceived.disconnect(messageHandler);
+
+        if (_virtualHoldController !== null) {
+            _virtualHoldController.cleanup();
+        }
+    }
 
     function attachmentAction(joint) {
         var attachmentData = getAttachmentData();
@@ -105,9 +296,9 @@
         if (joint === undefined) {
             if (attachmentData.joint.indexOf(LEFT_RIGHT_PLACEHOLDER) !== -1) {
                 joint = attachmentData.joint.replace(LEFT_RIGHT_PLACEHOLDER, _leftRightAttachToggle ? 'Left' : 'Right');
-                print('joint = ' + joint);
+                debugPrint('joint = ' + joint);
                 otherJoint = attachmentData.joint.replace(LEFT_RIGHT_PLACEHOLDER, _leftRightAttachToggle ? 'Right' : 'Left');
-                print('otherJoint = ' + otherJoint);
+                debugPrint('otherJoint = ' + otherJoint);
             } else {
                 joint = attachmentData.joint;
             }
@@ -127,7 +318,7 @@
         });
 
         if (attachmentData.action === 'attach') {
-            var attachmentModel = Entities.getEntityProperties(_entityID, 'modelURL').modelURL;
+            var attachmentModel = getEntityProperty(_entityID, 'modelURL');
             var newAttachment = {
                 jointName: joint,
                 modelUrl: attachmentModel
@@ -142,7 +333,7 @@
 
             if (ATTACH_SOUND.downloaded) {
                 Audio.playSound(ATTACH_SOUND, {
-                    position: Entities.getEntityProperties(_entityID, 'position').position,
+                    position: getEntityProperty(_entityID, 'position'),
                     volume: 0.2,
                     localOnly: true
                 });
@@ -156,7 +347,7 @@
         } else if (attachmentData.action === 'clear') {
             if (DETACH_SOUND.downloaded) {
                 Audio.playSound(DETACH_SOUND, {
-                    position: Entities.getEntityProperties(_entityID, 'position').position,
+                    position: getEntityProperty(_entityID, 'position'),
                     volume: 0.4,
                     localOnly: true
                 });
@@ -165,25 +356,47 @@
             if (otherJoint !== null) {
                 joints.push(joint);
             }
-            UserActivityLogger.logAction("attachmentItemScript_detach", {
+            UserActivityLogger.logAction('attachmentItemScript_detach', {
                 joints: joints
             });
         }
 
         MyAvatar.setAttachmentsVariant(newAttachments);        
     }
+
     this.startFarTrigger = function(entityID, args) {
         attachmentAction();
     };
 
+    this.startNearTrigger = function(entityID, args) {
+        var handJointName = args[0] === 'left' ? 'LeftHand': 'RightHand';
+        Messages.sendMessage(_listeningChannel, JSON.stringify({
+            action: 'create',
+            hand: args[0],
+            handPosition: MyAvatar.getJointPosition(handJointName),
+            handRotation: MyAvatar.getJointRotation(handJointName)
+        }));
+    };
+
     this.startNearGrab = function(entityID, args) {
-        print('NearGrabStarting ' + JSON.stringify(args));
+        debugPrint('NearGrabStarting ' + JSON.stringify(args));
         _isGrabbing = true;
         _attachmentData = getAttachmentData();
+
+        // send grab message if possible, to extend lifetime of object, after switching hand or giving attachment to other user:
+        try {
+            var attachmentServer = JSON.parse(getEntityProperty(entityID, 'userData')).attachmentServer;
+            if (attachmentServer !== undefined) {
+                Messages.sendMessage(attachmentServer, JSON.stringify({action: 'grab', entityID: entityID}));
+            }
+        } catch (e) {
+            // e
+        }
     };
+
     this.continueNearGrab = function(entityID, args) {
         if (_isGrabbing && _attachmentData !== null) {
-            print('NearGrabHappening ' + JSON.stringify(args));
+            // debugPrint('NearGrabHappening ' + JSON.stringify(args));
             var snapDistance = _attachmentData.snapDistance !== undefined ? _attachmentData.snapDistance :
                 DEFAULT_SNAP_DISTANCE;
             var entityPosition = Entities.getEntityProperties(entityID, 'position').position;
@@ -208,26 +421,25 @@
                 }
             });
 
-
             if (closestJoint !== null) {
-                print('attach' + closestJoint.name);
+                debugPrint('attach' + closestJoint.name);
                 attachmentAction(closestJoint.name);
                 Controller.triggerShortHapticPulse(0.3, args[0] === 'left' ? HAND_LEFT : HAND_RIGHT);
                 Entities.deleteEntity(_entityID);
             }
-            /*
-            if (Vec3.distance(entityPosition, targetJointPosition) < snapDistance) {
-                print('attach');
-                attachmentAction();
-                Entities.deleteEntity(_entityID);
-            }*/
         }
     };
+
     this.releaseGrab = function(entityID, args) {
         if (_isGrabbing) {
             _isGrabbing = false;
-            print('ReleaseGrab ' + JSON.stringify(args));
-            Entities.deleteEntity(_entityID);
+            debugPrint('ReleaseGrab ' + JSON.stringify(args));
+
+            // legacy behavior:
+            var name = getEntityProperty(entityID, 'name');
+            if (name !== undefined && name.toLowerCase().indexOf('clone') !== -1) {
+                Entities.deleteEntity(_entityID);
+            }
         }
     };
 
---
+++ Shared/attachmentsShelve/attachmentItemServerScript.js
@@ -0,0 +1,202 @@
+//
+//  attachmentItemServerScript.js
+//
+//  Created by Thijs Wenker on 8/21/17.
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+(function() {
+    var _entityID = null;
+    var _listeningChannel = null;
+    var _attachmentData = null;
+
+    // last created attachment per session
+    var _lastCreatedAttachments = {};
+
+    var WANT_DEBUG = true;
+
+    var MILLISECONDS_PER_SECOND = 1000;
+
+    // Can only create one entity per client in the period of MIN_CREATION_TIMEOUT in seconds
+    var MIN_CREATION_TIMEOUT = 5; // seconds
+
+    var LIFETIME_FROM_RELEASE = 2; // seconds
+
+    var TOTAL_HOLD_LIFETIME = 60; // seconds
+
+    var ATTACHMENT_CHANNEL_PREFIX = 'attachmentItem-';
+
+    var _scriptTimestamp = null;
+
+    var debugPrint = function(message) {};
+    if (WANT_DEBUG) {
+        debugPrint = function(message) {
+            print(_entityID + ': ' + message);
+        }
+    }
+
+    var getEntityProperty = function(entityID, property) {
+        return Entities.getEntityProperties(entityID, property)[property];
+    };
+
+    var getScriptTimestamp = function() {
+        return getEntityProperty(_entityID, 'scriptTimestamp');
+    }
+
+    var getUserData = function() {
+        try {
+            return JSON.parse(Entities.getEntityProperties(_entityID, 'userData').userData);
+        } catch (e) {
+            // e
+            debugPrint('Could not retrieve valid userData');
+        }
+        return null;
+    };
+
+    var getAttachmentData = function() {
+        var userDataObject = getUserData();
+        if (userDataObject === null || userDataObject.Attachment === undefined) {
+            return null;
+        }
+        return userDataObject.Attachment;
+    };
+
+    var messageHandler = function(channel, message, sender, localOnly) {
+        if (channel !== _listeningChannel) {
+            return;
+        }
+
+        // FIXME: entity server scripts don't always close down properly:
+        if (_scriptTimestamp !== getScriptTimestamp()) {
+            return;
+        }
+        
+        try {
+            var data = JSON.parse(message);
+            debugPrint('Message received: ' + message);
+            if (data.action === 'create') {
+                if (_lastCreatedAttachments[sender] !== undefined) {
+                    var creationTime = _lastCreatedAttachments[sender].createAt;
+                    var timespanSinceCreation = Date.now() - creationTime;
+                    if (timespanSinceCreation < (MILLISECONDS_PER_SECOND * MIN_CREATION_TIMEOUT)) {
+                        debugPrint('Throttling creation of attachment, another attachment has already been created for this session in the period of ' + MIN_CREATION_TIMEOUT + ' seconds.');
+                        return;
+                    }
+                }
+
+
+                debugPrint('trying to create entity');
+                var newProperties = Entities.getEntityProperties(_entityID);
+
+                var localPosition = Vec3.multiplyQbyV(Quat.inverse(newProperties.rotation),
+                    Vec3.subtract(data.handPosition, newProperties.position));
+
+                var localRotation = Quat.inverse(newProperties.rotation);
+                
+                newProperties.position = Vec3.sum(data.handPosition, localPosition);
+                newProperties.rotation = Quat.multiply(data.handRotation, localRotation);
+                newProperties.lifetime = TOTAL_HOLD_LIFETIME;
+
+                // delete some unused properties
+                delete newProperties.created;
+                delete newProperties.age;
+                delete newProperties.ageAsText;
+                if (newProperties.locked !== undefined) {
+                    delete newProperties.locked;
+                }
+
+                if (newProperties.renderInfo !== undefined) {
+                    delete newProperties.renderInfo;
+                }
+
+                delete newProperties.localRotation;
+                delete newProperties.localPosition;
+                
+                // We only want the server-side script in the locked item
+                if (newProperties.serverScripts !== undefined) {
+                    delete newProperties.serverScripts;
+                }
+
+                try {
+                    // attempt to modify the userData
+                    var userData = JSON.parse(newProperties.userData);
+                    userData.grabbableKey.wantsTrigger = false;
+                    userData.grabbableKey.grabbable = true;
+                    userData.attachmentServer = _listeningChannel;
+                    newProperties.userData = JSON.stringify(userData);
+                } catch (e) {
+                    debugPrint('Something went wrong while trying to modify the userData.');
+                }
+
+                // must be dynamic for hold action:
+                newProperties.dynamic = true;
+
+                if (newProperties.shapeType === undefined || newProperties.shapeType === 'none') {
+                    // must have dynamic shapeType for hold action:
+                    newProperties.shapeType = 'box';
+                }
+
+                var entityID = Entities.addEntity(newProperties);
+                Messages.sendMessage(_listeningChannel, JSON.stringify({
+                    action: 'attach',
+                    entityID: entityID,
+                    sessionUUID: sender,
+                    hand: data.hand,
+                    localPosition: localPosition,
+                    localRotation: localRotation
+                }));
+                _lastCreatedAttachments[sender] = {
+                    entityID: entityID,
+                    createdAt: Date.now()
+                };
+            } else if (data.action === 'release' && _lastCreatedAttachments[sender] !== undefined) {
+                if (_lastCreatedAttachments[sender].entityID !== data.entityID) {
+                    debugPrint(data.entityID + ' does not match previously created entity for session.');
+                    return;
+                }
+                // set lifetime to (entityProperties.age + LIFETIME_FROM_RELEASE)
+
+                var age = getEntityProperty(data.entityID, 'age');
+                debugPrint('Setting lifetime to release timeout');
+                Entities.editEntity(data.entityID, {lifetime: age + LIFETIME_FROM_RELEASE});
+            } else if (data.action === 'grab') {
+                var found = false;
+                Object.keys(_lastCreatedAttachments).forEach(function(session) {
+                    if (_lastCreatedAttachments[session].entityID === data.entityID) {
+                        found = true;
+                    }
+                });
+                if (!found) {
+                    debugPrint(data.entityID + ' not found in attachmentItemServer, ignoring grab message.');
+                    return;
+                }
+                // set lifetime back to TOTAL_HOLD_LIFETIME
+                debugPrint('Setting lifetime to default');
+                Entities.editEntity(data.entityID, {lifetime: TOTAL_HOLD_LIFETIME});
+            }
+        } catch (e) {
+
+        }
+    };
+
+    this.preload = function(entityID) {
+        _entityID = entityID;
+        _scriptTimestamp = getScriptTimestamp();
+        _listeningChannel = ATTACHMENT_CHANNEL_PREFIX + entityID;
+        Messages.subscribe(_listeningChannel);
+
+        debugPrint(_entityID + ': Connecting messageHandler.');
+        Messages.messageReceived.connect(messageHandler);
+    };
+
+    this.unload = function() {
+        debugPrint(_entityID + ': Disconnecting messageHandler.');
+        Messages.messageReceived.disconnect(messageHandler);
+    };
+
+
+})
+
