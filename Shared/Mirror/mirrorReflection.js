c60b845fac2ed6de91119abd43bf284b647271fa
RebeccaStankus <RebeccaStankus@users.noreply.github.com>
2017-09-07 12:15:32 -0700

    fixed syntax, indents, etc

--- Shared/Mirror/mirrorReflection.js
+++ Shared/Mirror/mirrorReflection.js
@@ -1,36 +1,35 @@
-//This script acts on the reflection box in front of the mirror so that when and avatar
-//enters the area, the mirror will reflect an image via the spectator camera
+//
+//  mirrorReflection.js
+//
+//  Created by Rebecca Stankus on 8/30/17.
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+// This script acts on the reflection box in front of the mirror so that when and avatar
+// enters the area, the mirror will reflect an image via the spectator camera
 
 (function () {
     print("Starting reflection script...............");
     var mirrorID, reflectionAreaID;
-   //get id of reflection area and mirror
+    // get id of reflection area and mirror
     this.preload = function(entityID) {
         reflectionAreaID = entityID;
         mirrorID = Entities.getEntityProperties(reflectionAreaID, 'parentID').parentID;
         print("Reflection area ID is : " + reflectionAreaID);
         print("Mirror area ID is : " + mirrorID);
-  };
+    };
 
-    //when avatar enters reflection area, begin reflecting
+    // when avatar enters reflection area, begin reflecting
     this.enterEntity = function(entityID){
-        
-      // var mirrorOverlayOn = function() {
-        //if(mirrorID.mirrorOverlayRunning == false) {
-            Entities.callEntityMethod(mirrorID, 'toggleMirrorOverlay');
-            print("Mirror is now reflecting.");
-      //  }
-      // }
-    }
+        Entities.callEntityMethod(mirrorID, 'toggleMirrorOverlay');
+        print("Mirror is now reflecting.");
+    };
 
-    //when avatar leaves reflection area, stop reflecting
+    // when avatar leaves reflection area, stop reflecting
     this.leaveEntity = function (entityID) {
-      //  var mirrorOverlayOff = function() {
-        //if(mirrorID.mirrorOverlayRunning == true) {
-            Entities.callEntityMethod(mirrorID, 'toggleMirrorOverlay');
-            print("Mirror is NOT reflecting anymore.");
-       // }
-            
-       // }
-    }
-})
+        Entities.callEntityMethod(mirrorID, 'toggleMirrorOverlay');
+        print("Mirror is NOT reflecting anymore.");
+    };
+});
