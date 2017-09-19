c73db724e13c41f75e7b069bfcf1bf3e53f17898
Elisa Lupin-Jimenez <elisa.lj11@gmail.com>
2017-09-19 11:10:15 -0700

    new textures/icons

---
+++ Marketplace/music-visualizer/textures/invisible.fs
@@ -0,0 +1,6 @@
+// Thoys presents you::::!!!
+// The Invisible shader:
+float getProceduralColors(inout vec3 diffuse, inout vec3 specular, inout float shininess) {    
+    discard;
+    return 1.0;
+}
