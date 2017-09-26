dfe87d13b420a6c840dccd1bf6baf894eec3e296
RebeccaStankus <RebeccaStankus@users.noreply.github.com>
2017-09-26 15:31:46 -0700

    finished the laundry

---
+++ Shared/Laundry/laundryZone.js
@@ -0,0 +1,15 @@
+//  laundryZone.js
+//
+//  Created by Rebecca Stankus on 9/25/17.
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+//  This script is applied to the dynamic laundry basket to keep it from moving when an item is thrown in.
+
+function filter(p, filterType) {
+    if (filterType === Entities.EDIT_FILTER_TYPE) {
+        return false;
+    }
+}
