c8613067804de7f33fd3f0b73569a3acabfbf546
RebeccaStankus <RebeccaStankus@users.noreply.github.com>
2017-09-28 13:00:04 -0700

    fixed description

--- DomainContent/AvatarStore/Laundry/laundryZone.js
+++ DomainContent/AvatarStore/Laundry/laundryZone.js
@@ -6,8 +6,8 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
-//  This script is applied to the dynamic laundry basket to keep it from moving when an item is thrown in.
-//  The script creates a zone around the basket with an edit reject filter to prevent any changes in position from being made.
+//  This script is applied to the zone around the dynamic laundry basket to keep it from moving when an item is thrown in.
+//  The script creates an edit reject filter to prevent any changes in position from being made.
 
 function filter(p, filterType) {
     if (filterType === Entities.EDIT_FILTER_TYPE) {
