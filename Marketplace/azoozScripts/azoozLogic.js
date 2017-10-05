b6cab091af23240a9becac60b8b20e5cfee9812f
pippilina <caitlyn@flutterverse.com>
2017-10-05 12:32:56 -0700

    Cleaned up with eslint

--- Marketplace/azoozScripts/azoozLogic.js
+++ Marketplace/azoozScripts/azoozLogic.js
@@ -14,68 +14,67 @@
 /* globals SoundCache Overlays HMD Messages */
 (function() {
 	
-	var _this = this;
-	var CHANNEL_PREFIX = 'io.highfidelity.azooz_server_';	
-	var COLLECT_CHANNEL = "collect";
-	var REGISTER_CHANNEL = "register";
+    var _this = this;
+    var COLLECT_CHANNEL = "collect";
+    var REGISTER_CHANNEL = "register";
 	
-	var QUOIN_SOUND = SoundCache.getSound("http://hifi-content.s3.amazonaws.com/DomainContent/production/azooz/cabled-mess__coin-c-02.wav");  
-    var VICTORYSOUND_SOURCE = SoundCache.getSound("http://hifi-content.s3.amazonaws.com/DomainContent/production/azooz/320887__rhodesmas__win-04.wav"));
+    var QUOIN_SOUND = SoundCache.getSound("http://hifi-content.s3.amazonaws.com/DomainContent/production/azooz/cabled-mess__coin-c-02.wav");  
+    var VICTORYSOUND_SOURCE = SoundCache.getSound("http://hifi-content.s3.amazonaws.com/DomainContent/production/azooz/320887__rhodesmas__win-04.wav");
 	
-	var scoreID = 0;
-	var inZone = false;	
-	var totalPickups = 0;
-	var score = 0;
-	var touchedPickups = [];
-	var registeredPickups = [];  
+    var scoreID = 0;
+    var totalPickups = 0;
+    var score = 0;
+    var touchedPickups = [];
+    var registeredPickups = [];  
   
-	var createDisplayTexts = function () {
-		scoreID = Overlays.addOverlay("text", {
-			width: 200,
-			height: 20,
-			text: "Score: 0/"+totalPickups,
-			alpha: 0.9,
-			backgroundAlpha: 1,
-			visible: true,
-			x: 80, y:40
-		});		
-	};
+    var createDisplayTexts = function () {
+        scoreID = Overlays.addOverlay("text", {
+            width: 200,
+            height: 20,
+            text: "Score: 0/"+totalPickups,
+            alpha: 0.9,
+            backgroundAlpha: 1,
+            visible: true,
+            x: 80, y:40
+        });		
+    };
 
 	
-	var onMessage = function(channel, message, sender) {  // channel string, messageis UID by default, sender is the avatar UID	
-        if (channel == REGISTER_CHANNEL) {		
-            if (registeredPickups.indexOf(message) == -1) {
-              registeredPickups.push(message);
-              totalPickups++;
-            } else {}
-            }			
-            if (channel == COLLECT_CHANNEL) {
-            if (touchedPickups.indexOf(message) == -1) {
-              touchedPickups.push(message);
-              score++;
-              Audio.playSound(QUOIN_SOUND, {
-                position: MyAvatar.position,
-                volume: 1,
-                localOnly: true
-			  });
-              
+    var onMessage = function(channel, message, sender) { // channel string, messageis UID by default, sender is the avatar UID	
+        if (channel === REGISTER_CHANNEL) {		
+            if (registeredPickups.indexOf(message) === -1) {
+                registeredPickups.push(message);
+                totalPickups++;
+            }
+        }			
+        if (channel === COLLECT_CHANNEL) {
+            if (touchedPickups.indexOf(message) === -1) {
+                touchedPickups.push(message);
+                score++;
+                Audio.playSound(QUOIN_SOUND, {
+                    position: MyAvatar.position,
+                    volume: 1,
+                    localOnly: true
+                });
             } 
         }
 		
-		Overlays.editOverlay(scoreID, {text : "Score: "+score+"/"+totalPickups});
+        Overlays.editOverlay(scoreID, {text : "Score: "+score+"/"+totalPickups});
 		
-		if (score == totalPickups) Audio.playSound(VICTORYSOUND_SOURCE, {
+        if (score === totalPickups) {
+            Audio.playSound(VICTORYSOUND_SOURCE, {
                 position: MyAvatar.position,
                 volume: 1,
                 localOnly: true
-			  });
-			  
+            });
+        }
+ 
     };				
 		
 			
     _this.preload = function(entityID) {
         HMD.displayModeChanged.connect(function() {
-			createDisplayTexts();        
+            createDisplayTexts();        
         });
         Messages.subscribe(COLLECT_CHANNEL);
         Messages.subscribe(REGISTER_CHANNEL);
@@ -89,15 +88,13 @@
     };
 
     _this.enterEntity = function(entityID) {
-        inZone = true;
-		createDisplayTexts();
+        createDisplayTexts();
     };
 
     _this.leaveEntity = function(entityID) {
         Messages.unsubscribe(COLLECT_CHANNEL);
         Messages.unsubscribe(REGISTER_CHANNEL);
         Overlays.deleteOverlay(scoreID);
-        inZone = false;		
     };
 
     _this.unload = _this.leaveEntity;
--- Marketplace/azoozScripts/azoozPickup-rupee.js
+++ Marketplace/azoozScripts/azoozPickup-rupee.js
@@ -13,52 +13,43 @@
 
 (function(){ 
 
-    //var CHANNEL_PREFIX = 'io.highfidelity.azooz_server_';
-	var COLLECT_CHANNEL = "collect";
-	var REGISTER_CHANNEL = "register";
-	// var STATUS_CHANNEL = "gamestatus";
-	var _entityID;	
-	var myProps;	
-	var _this = this;
-	var myOverlay;
-	var OVERLAYURL_NORMY = "http://hifi-content.s3.amazonaws.com/DomainContent/production/azooz/pickup/newRupee-green.fbx";
-	var OVERLAYURL_GHOSTY = "http://hifi-content.s3.amazonaws.com/DomainContent/production/azooz/pickup/rupee-ghost.fbx";
-	var collectableOverlay = null;
+    var COLLECT_CHANNEL = "collect";
+    var REGISTER_CHANNEL = "register";
+    var _entityID;
+    var _this = this;
+    var OVERLAYURL_NORMY = "http://hifi-content.s3.amazonaws.com/DomainContent/production/azooz/pickup/newRupee-green.fbx";
+    var OVERLAYURL_GHOSTY = "http://hifi-content.s3.amazonaws.com/DomainContent/production/azooz/pickup/rupee-ghost.fbx";
+    var collectableOverlay = null;
 	
-  this.preload = function(entityID) {
-    // Messages.subscribe(STATUS_CHANNEL);
-    // Messages.messageReceived.connect(onMessage);
-    collectableOverlay = Overlays.addOverlay('model', {
-        url: OVERLAYURL_NORMY,
-        localPosition: {x: 0, y: 0, z: 0},
-        localRotation: {x: 0, y: 0, z: 0, w: 1},
-        dimensions: {x: 0.2355, y: 0.943, z: 0.5216},
-        parentID: entityID
-    });    
-    _entityID = entityID;
-    _this.entityID = entityID;
-	myProps = Entities.getEntityProperties(_this.entityID, ['position', 'rotation', 'dimensions', 'userData']);		
-  };
+    this.preload = function(entityID) {
+        collectableOverlay = Overlays.addOverlay('model', {
+            url: OVERLAYURL_NORMY,
+            localPosition: {x: 0, y: 0, z: 0},
+            localRotation: {x: 0, y: 0, z: 0, w: 1},
+            dimensions: {x: 0.2355, y: 0.943, z: 0.5216},
+            parentID: entityID
+        });    
+        _entityID = entityID;
+        _this.entityID = entityID;
+    };
 
-  // Listen for a show/hide message
-  // Parent the zone to the rezzer
   
-  _this.enterEntity = function(entityID) { 
-      Messages.sendLocalMessage(COLLECT_CHANNEL, _this.entityID); // Inform the game manager that the avatar entered this pickup
-      Overlays.editOverlay(collectableOverlay, { // Ghost the pickup locally so only the player sees it ghosted, other players see it normal
-        url: OVERLAYURL_GHOSTY
-      });
-  }; 
+    _this.enterEntity = function(entityID) { 
+        Messages.sendLocalMessage(COLLECT_CHANNEL, _this.entityID); // Inform the game manager that the avatar entered this pickup
+        Overlays.editOverlay(collectableOverlay, { // Ghost the pickup locally so only the player sees it ghosted, other players see it normal
+            url: OVERLAYURL_GHOSTY
+        });
+    }; 
     
-  _this.unload = function() {		
-      Script.clearInterval(messageInterval);
-      if (collectableOverlay !== null) {
-        Overlays.deleteOverlay(collectableOverlay);
-      }
-  };
+    _this.unload = function() {		
+        Script.clearInterval(messageInterval);
+        if (collectableOverlay !== null) {
+            Overlays.deleteOverlay(collectableOverlay);
+        }
+    };
 	
-	var messageInterval = Script.setInterval(function(){ // Inform the game manager that this pickup exists, to add it to the total tally of pickups
+    var messageInterval = Script.setInterval(function(){ // Inform the game manager that this pickup exists, to add it to the total tally of pickups
         Messages.sendLocalMessage(REGISTER_CHANNEL, _entityID);
-  }, 10000);
+    }, 10000);
 		
 });
