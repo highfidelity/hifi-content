9d7349dc6aabff7d9cdce8ff5b7b642259cf5d2e
RebeccaStankus <RebeccaStankus@users.noreply.github.com>
2017-10-11 11:37:53 -0700

    cleaned up

--- DomainContent/AvatarStore/CheckoutZone/checkoutScanner.js
+++ DomainContent/AvatarStore/CheckoutZone/checkoutScanner.js
@@ -1,22 +1,30 @@
+//
+//  checkoutScanner.js
+//
+//  Created by Rebecca Stankus on 10/10/17.
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+//  This script acts on the scanner zone to pull up an item's marketplace page to enable purchasing.
 (function(){
-    
-    var _entityID;
+    var TABLET = Tablet.getTablet("com.highfidelity.interface.tablet.system");
     var interval;
     var properties;
     this.preload = function(entityID){
-        _entityID = entityID;
-        properties = Entities.getEntityProperties(_entityID, 'position');
+        properties = Entities.getEntityProperties(entityID, 'position');
         interval = Script.setInterval(function(){
-            var overlays = Overlays.findOverlays(properties.position, 0.25);
+            var overlays = Overlays.findOverlays(properties.position, 0.15);
             if (overlays.length > 0) {
-                print("I found you...");
                 overlays.forEach(function(overlay){
                     var name = Overlays.getProperty(overlay, 'name');
                     if (name.indexOf("MP") !== -1) {
+                        print("this one is MP!");
                         var id = name.substr(2,37);
+                        print("ID is " + id);
                         var goToURL = "https://metaverse.highfidelity.com/marketplace/items/" + id;
-                        var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
-                        tablet.gotoWebScreen(goToURL);
+                        TABLET.gotoWebScreen(goToURL);
                         Overlays.deleteOverlay(overlay);
                     }
                         
--- DomainContent/AvatarStore/CheckoutZone/checkoutZone.js
+++ DomainContent/AvatarStore/CheckoutZone/checkoutZone.js
@@ -7,25 +7,22 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
-//  This zone will provide an area at which a user may purchase an item. When the avatar enters the zone wearing a marketplace item, 
-//  the tablet will open to the marketplace home page for that item, allowing the user to quickly make the purchase.
+//  This zone will provide an area at which a user may purchase an item. When the avatar enters the zone wearing a 
+//  marketplace item, the item will appear as a small overlay. Scanning the overlay will cause the 
+//  the tablet to open to the marketplace home page for that item, allowing the user to quickly make the purchase.
 (function () {
-    print("Begin the checkout zone script take 3");
-    print("myavatarid is " + MyAvatar.sessionUUID);
     var SHARED = Script.require('./attachmentZoneShared.js');
-    var TABLE_PADDING = 0.3;
-    var ITEM_HEIGHT = 0.2;
+    var ITEM_HEIGHT = 0.1;
     
     var _this = this;
-    var tableProperties, tableHeight, tableLength, tableID, spawnPosition, spawnZ;
-    var zoneID, overlay;
-    var overlayList = [];
+    var tableProperties, tableHeight, tableLength, tableID, spawnZ, spawnY, spawnX;
+    var zoneID;
     var replicaList = [];
+    var left = true;
 
     this.preload = function(entityID) {
         zoneID = entityID;
     };
-    
 
     // Get info on checkout stand so we can place copies of items on it for purchasing
     // Find the position of the top of the stand at one end
@@ -33,33 +30,33 @@
         var zoneChildren = Entities.getChildrenIDs(zoneID);
         zoneChildren.forEach(function (childID) {
             var name = Entities.getEntityProperties(childID, 'name').name;
-            // print(name);
             if (name === "Checkout Table") {
-                // print("Found the checkout table .......");
                 tableProperties = Entities.getEntityProperties(childID, ['id','position', 'dimensions', 'rotation']);
                 tableID = tableProperties.id;
                 tableHeight = tableProperties.dimensions.y;
                 tableLength = tableProperties.dimensions.x;
-                spawnPosition = tableProperties.position;
-                spawnPosition.y += ((0.5 * tableHeight) + (0.5 * ITEM_HEIGHT));
-                spawnPosition.z += ((0.5 * tableLength) + TABLE_PADDING);
-                // print("Returning spawn position for checkout item");
+                var halfTableHeight = 0.5 * tableHeight;
+                var verticalSpace = 6 * ITEM_HEIGHT;
+                spawnY = halfTableHeight + verticalSpace;
+                var halfTableLength = 0.5 * tableLength;
+                spawnZ = (halfTableLength);
+                spawnX = 0;
                 return;
             }
         });
     });
 
     // Spawn a copy of each attachment scaled down to fit the ITEM_HEIGHT and place it on the checkout table
-    var spawnItem = (function(entityID){
-        // print("Making a copy for the checkout stand...");
-        var properties = Entities.getEntityProperties(entityID, ['name','modelURL','type','dimensions','marketplaceID']);
-        var oldName = properties.name;
-        properties.name = "Checkout Item " + oldName;
+    var spawnOverlayReplica = (function(entityID){
+        var properties = Entities.getEntityProperties(entityID, ['name','modelURL','type','dimensions','marketplaceID', 'modelURL']);
+        properties.url = properties.modelURL;
+        properties.name = "MP" + properties.marketplaceID;
+        properties.alpha = 1;
+        properties.grabbable = true;
         properties.parentID = tableID;
-        properties.localPosition = {"x":0,"y":((0.5 * tableHeight) + (0.5 * ITEM_HEIGHT)),"z":-spawnZ};
+        properties.localPosition = {"x":spawnX,"y":spawnY,"z":spawnZ};
         properties.localRotation = {"x":0,"y":0,"z":0};
-        properties.rotation = tableProperties.rotation;
-        properties.userData = "{\"grabbableKey\":{\"cloneable\":false,\"grabbable\":false}}";
+        properties.userData = "{\"grabbableKey\":{\"cloneable\":false,\"grabbable\":true}}";
         var scale = (ITEM_HEIGHT / properties.dimensions.y);
         if ((properties.dimensions.x > ITEM_HEIGHT) || (properties.dimensions.y > ITEM_HEIGHT) || (properties.dimensions.y > ITEM_HEIGHT)) {
             properties.dimensions.y = ITEM_HEIGHT;
@@ -67,67 +64,55 @@
             properties.dimensions.z *= scale;
         }
         // check that the item is not too large
-        while (properties.dimensions.x > 0.3 || properties.dimensions.z > 0.3) {
-            scale *= 0.8;
+        var maxItemSize = 0.3;
+        var scaleReduction;
+        while (properties.dimensions.x > maxItemSize || properties.dimensions.z > maxItemSize) {
+            scale *= scaleReduction;
             properties.dimensions.y *= scale;
             properties.dimensions.x *= scale;
             properties.dimensions.z *= scale;
         }
-        print("properties: " + JSON.stringify(properties));
-        Entities.addEntity(properties);
+        var replica = Overlays.addOverlay("model", properties);
+        replicaList.push(replica);
     });
 
     _this.enterEntity = (function (entityID) {
-        // print("You've entered the checkout zone");
+        left = true;
         getCheckoutStandPosition();
-        spawnZ = -((0.5 * tableLength) + ITEM_HEIGHT);
-        // print("got checkout stand position");
         SHARED.getAvatarChildEntities(MyAvatar).forEach(function (entityID) {
-            // print("Getting avatar child entiites...");
-            var childUserData = Entities.getEntityProperties(entityID, 'userData').userData;
-            var isAttachment = childUserData.indexOf("Attachment");
-            var marketplaceID = Entities.getEntityProperties(entityID, 'marketplaceID').marketplaceID;
-            print("item #: " + marketplaceID);
-            if (marketplaceID && (replicaList.indexOf(marketplaceID) === -1)) {
-                replicaList.push(marketplaceID);
+            var maxItems = 10;
+            if (replicaList.length < maxItems){
+                var childUserData = Entities.getEntityProperties(entityID, 'userData').userData;
+                var isAttachment = childUserData.indexOf("attached\":true");
+                var marketplaceID = Entities.getEntityProperties(entityID, 'marketplaceID').marketplaceID;
+                if (marketplaceID && (isAttachment !== -1)) {
                 
-                // TODO check for already purchased 
-                if (isAttachment !== -1) {
-                // print("Found an attachment from the marketplace.");
-                    spawnItem(entityID); // put a copy of the item on the table
-                    overlay = {
-                        type: "text",
-                        parentID: tableID,
-                        localPosition: {"x":ITEM_HEIGHT,"y":((0.5 * tableHeight) + (0.5 *ITEM_HEIGHT)),"z":-spawnZ},
-                        localRotation: {"x":0,"y":0,"z":0},
-                        name: marketplaceID,
-                        userData: "checkout",
-                        color: {
-                            red: 255,
-                            green: 255,
-                            blue: 255
-                        },
-                        alpha: 1,
-                        dimensions: {
-                            x: 0.01,
-                            y: 0.06,
-                            z: 0.12
-                        },
-                        solid: true,
-                        visible: true,
-                        lineWidth: 1.0,
-                        borderSize: 1.4
-                    };
-                    var tempOverlay = Overlays.addOverlay("cube", overlay); // add an overlay price tag
-                    overlayList.push(tempOverlay);
+                    // TODO check for already purchased 
+                    spawnOverlayReplica(entityID); // put a copy of the item on the table
                     // move spawn position over to the next empty spot
-                    spawnZ += TABLE_PADDING;
+                    var moveRight = 1.5 * ITEM_HEIGHT;
+                    var moveBack = 0.75 *ITEM_HEIGHT;
+                    var moveDown = 1.25 *ITEM_HEIGHT;
+                    var moveLeft = 1.5 *ITEM_HEIGHT;
+                    var moveForward = 0.75 *ITEM_HEIGHT;
+                    if (left) {
+                        spawnZ += moveRight;
+                        spawnX += moveBack;
+                        left = false;
+                    } else {
+                        spawnY -= moveDown;
+                        spawnZ -= moveLeft;
+                        spawnX -= moveForward;
+                        left = true;
+                    }
                 }
             }
 
             Overlays.mousePressOnOverlay.connect(function(overlayID, event){
-                if (overlayList.indexOf(overlayID) !== -1) {
-                    var goToURL = "https://metaverse.highfidelity.com/marketplace/items/" + Overlays.getProperty(overlayID, 'name');
+                if (replicaList.indexOf(overlayID) !== -1) {
+                    var name = Overlays.getProperty(overlayID, 'name');
+                    var id = name.substr(2,37);
+                    var goToURL = "https://metaverse.highfidelity.com/marketplace/items/" + id;
                     var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
                     tablet.gotoWebScreen(goToURL);
                 }
@@ -135,28 +120,7 @@
         });
     });
     
-    /* _this.findRayIntersection = (function(pickRay) {
-        var result = Overlays.findRayIntersection(pickRay);
-        if (result.intersects) {
-            var childOverlayID = result.overlayID;
-            var childOverlay = childOverlayID.getOverlayObject;
-            var selectedEntityID = childOverlay.parentID;
-            var goToID = Entities.getEntityProperties(selectedEntityID, 'marketplaceID').marketplaceID;
-            var goToURL = "https://metaverse.highfidelity.com/marketplace/items/" + goToID;
-            var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
-            tablet.gotoWebScreen(goToURL);
-            if (result.entityID === null) {
-                result.intersects = false;
-            }
-        }
-        return result;
-    });*/
-    Controller.Standard.RT = (function() {
-        print("You're getting warmer...");
-    });
-    
     _this.leaveEntity = function(){
-        replicaList = [];
         Entities.findEntities(MyAvatar.position, 1000).forEach(function(entity) {
             try {
                 var name = Entities.getEntityProperties(entity).name;
@@ -167,8 +131,9 @@
                 print("Error cleaning up.");
             }
         }); 
-        overlayList.forEach(function (overlayItem) {
+        replicaList.forEach(function (overlayItem) {
             Overlays.deleteOverlay(overlayItem);
         });
+        replicaList = [];
     };
 });
