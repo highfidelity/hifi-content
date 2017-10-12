31946697f5760ba603a548c352f05e3d83c7c006
Thijs Wenker <me@thoys.nl>
2017-10-12 23:01:29 +0200

    Check it out!

--- DomainContent/AvatarStore/CheckoutZone/checkoutScanner.js
+++ DomainContent/AvatarStore/CheckoutZone/checkoutScanner.js
@@ -8,33 +8,68 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 //  This script acts on the scanner zone to pull up an item's marketplace page to enable purchasing.
-(function(){
+(function() {
     var TABLET = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+    var MARKET_PLACE_ITEM_URL_PREFIX = 'https://metaverse.highfidelity.com/marketplace/items/';
+    var SCAN_RADIUS = 0.15; // meters
+    var OVERLAY_PREFIX = 'MP';
+    
     var interval;
     var properties;
-    this.preload = function(entityID){
-        properties = Entities.getEntityProperties(entityID, 'position');
-        interval = Script.setInterval(function(){
-            var overlays = Overlays.findOverlays(properties.position, 0.15);
+    var checkoutZoneID;
+
+    var scannedMPOverlays = {};
+
+    var onEntityAdded = function(entityID) {
+        print('entity added: ' + entityID);
+        var newItemProperties = Entities.getEntityProperties(entityID, ['marketplaceID', 'clientOnly', 'owningAvatarID', 'lastEditedBy']);
+        print(MyAvatar.sessionUUID);
+        print(newItemProperties.lastEditedBy);
+        if (/* newItemProperties.clientOnly && newItemProperties.owningAvatarID === MyAvatar.sessionUUID && */
+            (newItemProperties.lastEditedBy === MyAvatar.sessionUUID || newItemProperties.lastEditedBy === undefined) &&
+            newItemProperties.marketplaceID !== "") {
+            
+            print('Entity of interest added.');
+            if (scannedMPOverlays[newItemProperties.marketplaceID] !== undefined) {
+                print('scannedMPOverlays contains entity of interest.');
+                var overlayID = scannedMPOverlays[newItemProperties.marketplaceID];
+                Entities.callEntityMethod(checkoutZoneID, 'replicaCheckedOut', [overlayID, entityID]);
+                print('called the entity method.');
+                delete scannedMPOverlays[newItemProperties.marketplaceID];
+            }
+        }
+    };
+    
+    this.preload = function(entityID) {
+        properties = Entities.getEntityProperties(entityID, ['position', 'userData']);
+        try {
+            checkoutZoneID = JSON.parse(properties.userData).avatarStore.checkoutZoneID;
+            print('Checkout zone ID found: ' + checkoutZoneID);
+        } catch (e) {
+            print('Something went wrong trying to fetch the avatarStore.checkoutZoneID from the scanners userData.');
+        }
+        interval = Script.setInterval(function() {
+            var overlays = Overlays.findOverlays(properties.position, SCAN_RADIUS);
             if (overlays.length > 0) {
-                overlays.forEach(function(overlay){
+                overlays.forEach(function(overlay) {
                     var name = Overlays.getProperty(overlay, 'name');
-                    if (name.indexOf("MP") !== -1) {
+                    if (name.indexOf(OVERLAY_PREFIX) !== -1) {
                         print("this one is MP!");
-                        var id = name.substr(2,37);
-                        print("ID is " + id);
-                        var goToURL = "https://metaverse.highfidelity.com/marketplace/items/" + id;
+                        var marketplaceID = name.substr(OVERLAY_PREFIX.length, OVERLAY_PREFIX.length + 35);
+                        print("ID is " + marketplaceID);
+                        var goToURL = MARKET_PLACE_ITEM_URL_PREFIX + marketplaceID;
+                        scannedMPOverlays[marketplaceID] = overlay;
                         TABLET.gotoWebScreen(goToURL);
                         Overlays.deleteOverlay(overlay);
                     }
-                        
                 });
             }
         }, 1000);
+        Entities.addingEntity.connect(onEntityAdded);
     };
     
     this.unload = function() {
-        Script.clearInterface(interval);
+        Script.clearInterval(interval);
+        Entities.addingEntity.disconnect(onEntityAdded);
     };
-        
 });
--- DomainContent/AvatarStore/CheckoutZone/checkoutZone.js
+++ DomainContent/AvatarStore/CheckoutZone/checkoutZone.js
@@ -12,16 +12,52 @@
 //  the tablet to open to the marketplace home page for that item, allowing the user to quickly make the purchase.
 (function () {
     var SHARED = Script.require('./attachmentZoneShared.js');
+    var MARKET_PLACE_ITEM_URL_PREFIX = 'https://metaverse.highfidelity.com/marketplace/items/';
     var ITEM_HEIGHT = 0.1;
+    var OVERLAY_PREFIX = 'MP';
+    var TRANSFORMS_SETTINGS = 'io.highfidelity.avatarStore.checkOut.tranforms';
     
     var _this = this;
+    var isInZone = false;
     var tableProperties, tableHeight, tableLength, tableID, spawnZ, spawnY, spawnX;
     var zoneID;
     var replicaList = [];
+    var replicaStoredTransforms = {};
     var left = true;
 
     this.preload = function(entityID) {
         zoneID = entityID;
+    };
+
+    var getTransformForMarketplaceItems = function() {
+        return Settings.getValue(TRANSFORMS_SETTINGS, {});
+    };
+    
+    var getTransformsForMarketplaceItem = function(marketplaceID) {
+        var transformItems = getTransformForMarketplaceItems();
+        if (transformItems[marketplaceID] === undefined) {
+            return {
+                certificateTransforms: {},
+                unsortedTransforms: [],
+                lastUsedUnsortedTransformIndex: -1
+            };
+        }
+        return transformItems[marketplaceID];
+    };
+    
+    var addTransformForMarketplaceItem = function(marketplaceID, certificateID, transform) {
+        if (marketplaceID === undefined) {
+            return;
+        }
+        var marketplaceItemTransforms = getTransformForMarketplaceItems();
+        var marketplaceItemTransform = getTransformsForMarketplaceItem(marketplaceID);
+        if (certificateID !== undefined) {
+            marketplaceItemTransform.certificateTransforms[certificateID] = transform;
+        } else {
+            marketplaceItemTransform.unsortedTransforms.push(transform);
+        }
+        marketplaceItemTransforms[marketplaceID] = marketplaceItemTransform;
+        Settings.setValue(TRANSFORMS_SETTINGS, marketplaceItemTransforms);
     };
 
     // Get info on checkout stand so we can place copies of items on it for purchasing
@@ -31,7 +67,7 @@
         zoneChildren.forEach(function (childID) {
             var name = Entities.getEntityProperties(childID, 'name').name;
             if (name === "Checkout Table") {
-                tableProperties = Entities.getEntityProperties(childID, ['id','position', 'dimensions', 'rotation']);
+                tableProperties = Entities.getEntityProperties(childID, ['id', 'position', 'dimensions', 'rotation']);
                 tableID = tableProperties.id;
                 tableHeight = tableProperties.dimensions.y;
                 tableLength = tableProperties.dimensions.x;
@@ -47,36 +83,107 @@
     });
 
     // Spawn a copy of each attachment scaled down to fit the ITEM_HEIGHT and place it on the checkout table
-    var spawnOverlayReplica = (function(entityID){
-        var properties = Entities.getEntityProperties(entityID, ['name','modelURL','type','dimensions','marketplaceID', 'modelURL']);
-        properties.url = properties.modelURL;
-        properties.name = "MP" + properties.marketplaceID;
-        properties.alpha = 1;
-        properties.grabbable = true;
-        properties.parentID = tableID;
-        properties.localPosition = {"x":spawnX,"y":spawnY,"z":spawnZ};
-        properties.localRotation = {"x":0,"y":0,"z":0};
-        properties.userData = "{\"grabbableKey\":{\"cloneable\":false,\"grabbable\":true}}";
-        var scale = (ITEM_HEIGHT / properties.dimensions.y);
-        if ((properties.dimensions.x > ITEM_HEIGHT) || (properties.dimensions.y > ITEM_HEIGHT) || (properties.dimensions.y > ITEM_HEIGHT)) {
-            properties.dimensions.y = ITEM_HEIGHT;
-            properties.dimensions.x *= scale;
-            properties.dimensions.z *= scale;
+    var spawnOverlayReplica = (function(entityID) {
+        var entityProperties = Entities.getEntityProperties(entityID, [
+            'name', 'modelURL', 'type', 'dimensions', 'marketplaceID', 'modelURL',
+            'localPosition', 'localRotation', 'dimensions', 'parentJointIndex'
+        ]);
+        var overlayProperties = {
+            url: entityProperties.modelURL,
+            name: OVERLAY_PREFIX + entityProperties.marketplaceID,
+            alpha: true,
+            grabbable: true,
+            parentID: tableID,
+            localPosition: {x: spawnX, y: spawnY, z: spawnZ},
+            localRotation: {x: 0, y: 0, z: 0},
+            // clone dimensions so we can alter it without messing up the original entities dimensions
+            dimensions: JSON.parse(JSON.stringify(entityProperties.dimensions))
+        };
+        var scale = (ITEM_HEIGHT / overlayProperties.dimensions.y);
+        if ((overlayProperties.dimensions.x > ITEM_HEIGHT) || (overlayProperties.dimensions.y > ITEM_HEIGHT) || (overlayProperties.dimensions.y > ITEM_HEIGHT)) {
+            overlayProperties.dimensions.y = ITEM_HEIGHT;
+            overlayProperties.dimensions.x *= scale;
+            overlayProperties.dimensions.z *= scale;
         }
         // check that the item is not too large
         var maxItemSize = 0.3;
         var scaleReduction;
-        while (properties.dimensions.x > maxItemSize || properties.dimensions.z > maxItemSize) {
+        while (overlayProperties.dimensions.x > maxItemSize || overlayProperties.dimensions.z > maxItemSize) {
             scale *= scaleReduction;
-            properties.dimensions.y *= scale;
-            properties.dimensions.x *= scale;
-            properties.dimensions.z *= scale;
+            overlayProperties.dimensions.y *= scale;
+            overlayProperties.dimensions.x *= scale;
+            overlayProperties.dimensions.z *= scale;
         }
-        var replica = Overlays.addOverlay("model", properties);
+        var replica = Overlays.addOverlay("model", overlayProperties);
+        var replicaStoredTransform = {
+            position: entityProperties.localPosition,
+            rotation: entityProperties.localRotation,
+            dimensions: entityProperties.dimensions,
+            jointName: MyAvatar.jointNames[entityProperties.parentJointIndex],
+            demoEntityID: entityID
+        };
+
+        print('stored transform = ' + JSON.stringify(replicaStoredTransform));
+
+        replicaStoredTransforms[replica] = replicaStoredTransform;
         replicaList.push(replica);
     });
 
+    _this.replicaCheckedOut = function(entityID, args) {
+        var ARGS_INDEX = {
+            REPLICA_OVERLAY: 0,
+            NEW_ENTITY: 1
+        };
+        var replicaOverlayID = args[ARGS_INDEX.REPLICA_OVERLAY];
+        var newEntityID = args[ARGS_INDEX.NEW_ENTITY];
+        print('replicaOverlayID = ' + replicaOverlayID);
+        print('newEntityID = ' + newEntityID);
+        
+        // Delete the new entity when the transforms are not found.
+        if (replicaStoredTransforms[replicaOverlayID] === undefined) {
+            print('Could not find transform data, deleting purchased entity.');
+            Entities.deleteEntity(newEntityID);
+            return;
+        }
+
+        var transform = replicaStoredTransforms[replicaOverlayID];
+        var transformProperties = {
+            parentID: MyAvatar.sessionUUID,
+            parentJointIndex: MyAvatar.getJointIndex(transform.jointName),
+            localPosition: transform.position,
+            localRotation: transform.rotation,
+            dimensions: transform.dimensions,
+            velocity: {x: 0, y: 0, z: 0},
+            dynamic: false
+        };
+        Entities.editEntity(newEntityID, transformProperties);
+
+        // Make really sure that the translations are set properly
+        var makeSureInterval = Script.setInterval(function() {
+            Entities.editEntity(newEntityID, transformProperties);
+        }, 100);
+
+        // Five seconds should be enough to be sure, otherwise we have a problem
+        Script.setTimeout(function() {
+            makeSureInterval.stop();
+        }, 5000);
+
+        print('Entities.editEntity(newEntityID, transformProperties): ' + newEntityID + ', ' + JSON.stringify(transformProperties));
+
+
+        var newEntityProperties = Entities.getEntityProperties(newEntityID, ['marketplaceID', 'certificateID']);
+        var certificateID = undefined;
+        if (newEntityProperties.certificateID !== "" && newEntityProperties.certificateID !== undefined) {
+            certificateID = newEntityProperties.certificateID;
+        }
+        addTransformForMarketplaceItem(newEntityProperties.marketplaceID, certificateID, transform);
+
+        // Remove the demo object, to prevent overlapping objects
+        Entities.deleteEntity(transform.demoEntityID);
+    };
+
     _this.enterEntity = (function (entityID) {
+        isInZone = true;
         left = true;
         getCheckoutStandPosition();
         SHARED.getAvatarChildEntities(MyAvatar).forEach(function (entityID) {
@@ -91,10 +198,10 @@
                     spawnOverlayReplica(entityID); // put a copy of the item on the table
                     // move spawn position over to the next empty spot
                     var moveRight = 1.5 * ITEM_HEIGHT;
-                    var moveBack = 0.75 *ITEM_HEIGHT;
-                    var moveDown = 1.25 *ITEM_HEIGHT;
-                    var moveLeft = 1.5 *ITEM_HEIGHT;
-                    var moveForward = 0.75 *ITEM_HEIGHT;
+                    var moveBack = 0.75 * ITEM_HEIGHT;
+                    var moveDown = 1.25 * ITEM_HEIGHT;
+                    var moveLeft = 1.5 * ITEM_HEIGHT;
+                    var moveForward = 0.75 * ITEM_HEIGHT;
                     if (left) {
                         spawnZ += moveRight;
                         spawnX += moveBack;
@@ -111,8 +218,8 @@
             Overlays.mousePressOnOverlay.connect(function(overlayID, event){
                 if (replicaList.indexOf(overlayID) !== -1) {
                     var name = Overlays.getProperty(overlayID, 'name');
-                    var id = name.substr(2,37);
-                    var goToURL = "https://metaverse.highfidelity.com/marketplace/items/" + id;
+                    var id = name.substr(OVERLAY_PREFIX.length, OVERLAY_PREFIX.length + 35);
+                    var goToURL = MARKET_PLACE_ITEM_URL_PREFIX + id;
                     var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
                     tablet.gotoWebScreen(goToURL);
                 }
@@ -120,7 +227,8 @@
         });
     });
     
-    _this.leaveEntity = function(){
+    _this.leaveEntity = function() {
+        isInZone = false;
         Entities.findEntities(MyAvatar.position, 1000).forEach(function(entity) {
             try {
                 var name = Entities.getEntityProperties(entity).name;
@@ -135,5 +243,13 @@
             Overlays.deleteOverlay(overlayItem);
         });
         replicaList = [];
+        replicaStoredTransforms = {};
     };
+
+    _this.unload = function() {
+        // sure you leave the entity if you're still in there
+        if (isInZone) {
+            _this.leaveEntity();
+        }
+    }
 });
