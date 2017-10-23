29a1a8c8f4e4f3eec7045a0f3a701efd10202290
Thijs Wenker <me@thoys.nl>
2017-10-24 00:17:17 +0200

    removed commented out prints

--- DomainContent/AvatarStore/CheckoutZone/checkoutScanner.js
+++ DomainContent/AvatarStore/CheckoutZone/checkoutScanner.js
@@ -23,20 +23,14 @@
     var scannedMPOverlays = {};
 
     var onEntityAdded = function(entityID) {
-        // print('entity added: ' + entityID);
         var newItemProperties = Entities.getEntityProperties(entityID, ['marketplaceID', 'clientOnly', 'owningAvatarID', 'lastEditedBy']);
-        // print(MyAvatar.sessionUUID);
-        //  print(newItemProperties.lastEditedBy);
         if (/* newItemProperties.clientOnly && newItemProperties.owningAvatarID === MyAvatar.sessionUUID && */
             (newItemProperties.lastEditedBy === MyAvatar.sessionUUID || newItemProperties.lastEditedBy === undefined) &&
             newItemProperties.marketplaceID !== "") {
-            
-            // print('Entity of interest added.');
+
             if (scannedMPOverlays[newItemProperties.marketplaceID] !== undefined) {
-                // print('scannedMPOverlays contains entity of interest.');
                 var overlayID = scannedMPOverlays[newItemProperties.marketplaceID];
                 Entities.callEntityMethod(checkoutZoneID, 'replicaCheckedOut', [overlayID, entityID]);
-                // print('called the entity method.');
                 delete scannedMPOverlays[newItemProperties.marketplaceID];
             }
         }
@@ -46,7 +40,6 @@
         properties = Entities.getEntityProperties(entityID, ['position', 'userData']);
         try {
             checkoutZoneID = JSON.parse(properties.userData).avatarStore.checkoutZoneID;
-            //  print('Checkout zone ID found: ' + checkoutZoneID);
         } catch (e) {
             print('Something went wrong trying to fetch the avatarStore.checkoutZoneID from the scanners userData.');
         }
@@ -56,9 +49,7 @@
                 overlays.forEach(function(overlay) {
                     var name = Overlays.getProperty(overlay, 'name');
                     if (name.indexOf(OVERLAY_PREFIX) !== -1) {
-                        //  print("this one is MP!");
                         var marketplaceID = name.substr(OVERLAY_PREFIX.length, OVERLAY_PREFIX.length + 35);
-                        // print("ID is " + marketplaceID);
                         var goToURL = MARKET_PLACE_ITEM_URL_PREFIX + "/items/" + marketplaceID;
                         scannedMPOverlays[marketplaceID] = overlay;
                         TABLET.gotoWebScreen(goToURL, MARKETPLACES_INJECT_SCRIPT_URL);
