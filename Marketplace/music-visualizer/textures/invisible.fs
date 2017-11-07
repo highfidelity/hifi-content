4ba533abb1cbff7a8dca110431474902c54698d2
Elisa Lupin-Jimenez <elisa@highfidelity.io>
2017-11-07 11:10:25 -0800

    temporarily removed musvis for emoji PR

--- Marketplace/music-visualizer/textures/invisible.fs
+++
@@ -1,6 +0,0 @@
-// Thoys presents you::::!!!
-// The Invisible shader:
-float getProceduralColors(inout vec3 diffuse, inout vec3 specular, inout float shininess) {    
-    discard;
-    return 1.0;
-}
