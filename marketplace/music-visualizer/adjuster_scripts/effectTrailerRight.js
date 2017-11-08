79db88524feacc895d9221e3703cbfc34aea38da
Elisa Lupin-Jimenez <elisa@highfidelity.io>
2017-11-07 16:52:29 -0800

    removed emoji momentarily, re-added musvis, fixed browse system (dependent on OffscreenUi.cpp update)

---
+++ marketplace/music-visualizer/adjuster_scripts/effectTrailerRight.js
@@ -0,0 +1,55 @@
+//  
+//  effectTrailerRight.js
+//  An entity script to track right trigger holds to create trails in a particle effect
+//  
+//  Author: Elisa Lupin-Jimenez
+//  Copyright High Fidelity 2017
+// 
+//  Licensed under the Apache 2.0 License
+//  See accompanying license file or http://apache.org/
+//  
+//  All assets are under CC Attribution Non-Commerical
+//  http://creativecommons.org/licenses/
+//  
+
+(function() {
+    var _this = this;
+
+    _this.preload = function(entityID) {
+        print("Right trailer script has loaded");
+        _this.entityID = entityID;
+    };
+
+    // The name of the new mapping
+    var MAPPING_NAME = "com.highfidelity.controllers.example.triggerExample";
+
+    // Create a new mapping object
+    var mapping = Controller.newMapping(MAPPING_NAME);
+    var oldPressStrength = 0;
+    var pressStrength = 0;
+
+    mapping.from(Controller.Standard.RT).to(function(value) {
+        var props = Entities.getEntityProperties(_this.entityID, 'isEmitting');
+        pressStrength = value;
+        if (pressStrength < 0.01 && oldPressStrength >= 0.01) {
+            props.isEmitting = false;
+            Entities.editEntity(_this.entityID, props);
+        }
+
+        if (pressStrength >= 0.01 && oldPressStrength < 0.01) {
+            props.isEmitting = true;
+            Entities.editEntity(_this.entityID, props);
+        }
+
+        oldPressStrength = pressStrength;
+    });
+
+    Controller.enableMapping(MAPPING_NAME);
+
+    // Disable the new mapping when the script ends
+    Script.scriptEnding.connect(function () {
+        Controller.disableMapping(MAPPING_NAME);
+    });
+
+
+});
