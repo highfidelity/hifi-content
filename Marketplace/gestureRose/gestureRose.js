607b99976fc143b5632e506db52843ca797b04f4
Fluffy Jenkins <richardjrn.w@gmail.com>
2018-01-11 19:58:38 +0000

    Made changes to script based on QA
    0.45m trigger ammount
    Works on both hands
    Grip pressure adjusted to 0.75

--- Marketplace/gestureRose/gestureRose.js
+++ Marketplace/gestureRose/gestureRose.js
@@ -1,5 +1,5 @@
 var X_TRIGGER_LIMIT = 0.1;
-var Y_TRIGGER_DIST = -0.2;
+var Y_TRIGGER_DIST = -0.145;
 var Z_TRIGGER_LIMIT = 0.1;
 var GLOBAL_UP = {x: 0, y: 1, z: 0};
 var DEG_TO_RAD = Math.PI / 180;
@@ -13,6 +13,7 @@ var ROSE_URL = Script.resolvePath('longStemRose.fbx');
 
 var gestureStarted = false;
 var gestureStartPos = null;
+var _gestureHand = 0;
 
 Script.scriptEnding.connect(function () {
     shutdown();
@@ -20,13 +21,26 @@ Script.scriptEnding.connect(function () {
 
 controllerMappingName = 'gestureRoseMapping';
 controllerMapping = Controller.newMapping(controllerMappingName);
+controllerMapping.from(Controller.Standard.LeftGrip).to(function (value) {
+    gestureLogic(value,-1);
+});
 controllerMapping.from(Controller.Standard.RightGrip).to(function (value) {
-    if (value === 1 && isPalmUpwards()) {
+    gestureLogic(value,1);
+});
+
+function gestureLogic(value,gestureHand){
+    var jointName = "LeftHand";
+    if(gestureHand === 1){
+        jointName = "RightHand";
+    }
+
+    if (value >= 0.7 && isPalmUpwards(gestureHand)) {
+        _gestureHand = gestureHand;
         gestureStarted = true;
-        gestureStartPos = MyAvatar.getJointPosition("RightHand");
+        gestureStartPos = MyAvatar.getJointPosition(jointName);
     } else if (value === 0) {
-        if (isPalmUpwards() && gestureStarted) {
-            var gestureEndPos = MyAvatar.getJointPosition("RightHand");
+        if (isPalmUpwards(gestureHand) && gestureStarted && gestureHand === _gestureHand) {
+            var gestureEndPos = MyAvatar.getJointPosition(jointName);
             var total = Vec3.subtract(gestureEndPos, gestureStartPos);
             if (Math.abs(total.x) < X_TRIGGER_LIMIT && total.y < Y_TRIGGER_DIST && Math.abs(total.z) < Z_TRIGGER_LIMIT) {
                 var clientOnly = !(Entities.canRez() || Entities.canRezTmp());
@@ -43,19 +57,24 @@ controllerMapping.from(Controller.Standard.RightGrip).to(function (value) {
                     userData: "{ \"grabbableKey\": { \"grabbable\": true, \"kinematic\": false } }"
                 }, clientOnly);
                 gestureStarted = false;
+                _gestureHand = 0;
             }
         } else if (gestureStarted) {
+            _gestureHand = 0;
             gestureStarted = false;
         }
     }
-});
-
+}
 
 init();
 
-function isPalmUpwards() {
+function isPalmUpwards(gestureHand) {
+    var handRot = MyAvatar.getLeftHandPose()["rotation"];
+    if(gestureHand === 1){
+        handRot = MyAvatar.getRightHandPose()["rotation"];
+    }
     var returnVal = false;
-    var normal = Vec3.multiplyQbyV(Quat.multiply(MyAvatar.orientation, MyAvatar.getRightHandPose()["rotation"]), {
+    var normal = Vec3.multiplyQbyV(Quat.multiply(MyAvatar.orientation, handRot), {
         x: 0,
         y: 0,
         z: 1
