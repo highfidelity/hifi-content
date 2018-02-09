1ad1d78d5a37bf120db9044c824f27714575f954
Liv Erickson <liv@highfidelity.io>
2018-02-09 12:00:22 -0800

    fixed issue with chat not displaying overhead

--- DomainContent/Start/support.js
+++ DomainContent/Start/support.js
@@ -46,7 +46,6 @@
     var speechBubbleOffset = {x: 0, y: 0.3, z: 0.0}; // The offset from the joint to whic the speech bubble is attached.
     var speechBubbleJointName = 'Head'; // The name of the joint to which the speech bubble is attached.
     var speechBubbleLineHeight = 0.05; // The height of a line of text in the speech bubble.
-    var SPEECH_BUBBLE_MAX_WIDTH = 1; // meters
 
     // Load the persistent variables from the Settings, with defaults.
     function loadSettings() {
@@ -583,16 +582,8 @@
         }
 
         var fudge = 0.02;
-
         var width = textSize.width + fudge;
-        var height = speechBubbleLineHeight + fudge;
-
-        if (textSize.width >= SPEECH_BUBBLE_MAX_WIDTH) {
-            var numLines = Math.ceil(width);
-            height = speechBubbleLineHeight * numLines + fudge;
-            width = SPEECH_BUBBLE_MAX_WIDTH;
-        } 
-
+        var height = textSize.height + fudge;
         dimensions = {
             x: width,
             y: height,
@@ -618,7 +609,6 @@
             Vec3.sum(
                 headPosition,
                 rotatedOffset);
-        position.y += height / 2; // offset based on half of bubble height
         speechBubbleParams.position = position;
 
         if (!speechBubbleTextID) {
