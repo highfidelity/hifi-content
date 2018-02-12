0d4a7461cf47506a95b183aea1df0c0ccc1de384
Elisa Lupin-Jimenez <elisa@highfidelity.io>
2018-02-12 12:11:23 -0800

    updated crunch.js

--- DomainContent/Mexico/crunch.js
+++ DomainContent/Mexico/crunch.js
@@ -7,15 +7,17 @@
 // License: Attribution 3.0 | Recorded by Simon Craggs
 //
 (function () {
-    var CRUNCH_SOUND_URL = "https://hifi-content.s3.amazonaws.com/liv/dev/emojis/Apple_Bite-Simon_Craggs-1683647397.wav";
+    var CRUNCH_SOUND_URL = Script.resolvePath("./sounds/Apple_Bite-Simon_Craggs-1683647397.wav");
     var CRUNCH = SoundCache.getSound(Script.resolvePath(CRUNCH_SOUND_URL));
-    var playback; 
+    var VOLUME = 0.5;
+    var WITHIN_10_CM = 0.1;
+    var playback;
     var _this = this;
+
     _this.preload = function (entityID) {
         _this.entityID = entityID;
-        var props = Entities.getEntityProperties(entityID);
-        playback = {volume: 0.5, position: Entities.getEntityProperties(_this.entityID).position};
-    }
+        playback = {volume: VOLUME, position: Entities.getEntityProperties(_this.entityID).position};
+    };
 
     var checkIfNearHead = function () {
         var pos = Entities.getEntityProperties(_this.entityID).position;
@@ -24,24 +26,23 @@
             isWithin10cm(pos.z, avatarHeadPosition.z)) {
             playEatingEffect(pos);
         }
-    }
+    };
 
     var playEatingEffect = function (pos) {
- 
-        print("Crunch! Eating " + _this.entityID);
         Audio.playSound(CRUNCH, playback);
         Entities.deleteEntity(_this.entityID);
-    }
+    };
 
     // Helper function to see if the object is close to us
     var isWithin10cm = function (val1, val2) {
-        if (Math.abs(Math.abs(val1) - Math.abs(val2)) <= .1)
+        if (Math.abs(Math.abs(val1) - Math.abs(val2)) <= WITHIN_10_CM) {
             return true;
+        }
 
         return false;
-    }
+    };
     Script.update.connect(checkIfNearHead);
     _this.unload = function (entityID) {
         Script.update.disconnect(checkIfNearHead);
-    }
-})
+    };
+});
