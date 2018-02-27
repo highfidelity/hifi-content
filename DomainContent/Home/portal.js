bcaa75fcdffe8e1c7e3f8b931061d5d6f826369b
Flame Soulis <firecatdan@gmail.com>
2018-02-26 23:36:59 -0500

    Updated portal.js from S3 and fixed bug 3374

--- DomainContent/Home/portal.js
+++ DomainContent/Home/portal.js
@@ -1,52 +1,93 @@
-(function(){
-    var teleport;
+(function () {
+    var teleportSound;
     var portalDestination;
-    var thisEntityID;
+    var animationURL;
+    var position;
+    var canTeleport;
 
-    function playSound() {
-        var properties = Entities.getEntityProperties(thisEntityID, 'position');
-        if (properties) {
-            Audio.playSound(teleport, { position: properties.position, volume: 0.40, localOnly: true });
-        }
-    };
-
-    this.preload = function(entityID) {
-        thisEntityID = entityID;
-        teleport = SoundCache.getSound("atp:/sounds/teleport.raw");
-
-        var properties = Entities.getEntityProperties(entityID, 'userData');
-        if (properties) {
-            portalDestination = properties.userData;
-            print("portal.js | The portal destination is " + portalDestination);
+    function playSound(entityID) {
+        print("playing teleport sound");
+        if (teleportSound.downloaded) {
+            if (!position) {
+                getProps(entityID);
+            }
+            Audio.playSound(teleportSound, { position: position, volume: 0.40, localOnly: true });
         }
     }
 
-    this.enterEntity = function(entityID) {
-        print("portal.js | enterEntity");
-
-        var properties = Entities.getEntityProperties(entityID, 'userData'); // in case the userData/portalURL has changed
+    function getProps(entityID) {
+        var properties = Entities.getEntityProperties(entityID);
         if (properties) {
+            animationURL = properties.modelURL;
+            position = properties.position;
             portalDestination = properties.userData;
-
-            print("portal.js | enterEntity() .... The portal destination is " + portalDestination);
-
-            if (portalDestination.length > 0) {
-                if (portalDestination[0] == '/') {
-                    print("Teleporting to " + portalDestination);
-                    Window.location = portalDestination;
-                } else {
-                    print("Teleporting to hifi://" + portalDestination);
-                    Window.location = "hifi://" + portalDestination;
-                }
-            } else {
-                location.goToEntry(); // going forward: no data means go to appropriate entry point
-            }
         }
+    }
+
+    function isPositionInsideBox(position, boxProperties) {
+        var localPosition = Vec3.multiplyQbyV(Quat.inverse(boxProperties.rotation),
+            Vec3.subtract(MyAvatar.position, boxProperties.position));
+        var halfDimensions = Vec3.multiply(boxProperties.dimensions, 0.5);
+        return -halfDimensions.x <= localPosition.x &&
+            halfDimensions.x >= localPosition.x &&
+            -halfDimensions.y <= localPosition.y &&
+            halfDimensions.y >= localPosition.y &&
+            -halfDimensions.z <= localPosition.z &&
+            halfDimensions.z >= localPosition.z;
+    }
+
+    this.preload = function (entityID) {
+        print("loading teleport script");
+        teleportSound = SoundCache.getSound("http://s3.amazonaws.com/hifi-public/birarda/teleport.raw");
+        getProps(entityID);
+        // check if we are already in the object
+        canTeleport = !isPositionInsideBox(MyAvatar.position, Entities.getEntityProperties(entityID));
     };
 
-    this.leaveEntity = function(entityID) {
-        print("portal.js | leaveEntity");
+    this.enterEntity = function (entityID) {
+        // check if we should teleport
+        if (canTeleport === false) {
+            // if we have not passed enough time, do not teleport the user
+            return;
+        }
+        // get latest props in case we changed the destination
+        getProps(entityID);
+        print("enterEntity() .... The portal destination is " + portalDestination);
 
-        playSound();
+        if (portalDestination.length > 0) {
+            print("Teleporting to hifi://" + portalDestination);
+            Window.location = "hifi://" + portalDestination;
+        }
+
     };
-})
+
+    this.leaveEntity = function (entityID) {
+        print("leaveEntity() called ....");
+        if (!animationURL) {
+            getProps(entityID);
+        }
+        Entities.editEntity(entityID, {
+            animation: { url: animationURL, currentFrame: 1, running: false }
+        });
+
+        if (canTeleport === true) {
+            // only play the sound if we can teleport
+            playSound(entityID);
+        }
+        canTeleport = true;
+    };
+
+    this.hoverEnterEntity = function (entityID) {
+        print("hoverEnterEntity() called ....");
+        if (!animationURL) {
+            getProps(entityID);
+        }
+        Entities.editEntity(entityID, {
+            animation: { url: animationURL, fps: 24, firstFrame: 1, lastFrame: 25, currentFrame: 1, running: true, hold: true }
+        });
+    };
+
+    this.unload = function () {
+        print("unloading teleport script");
+    };
+});
