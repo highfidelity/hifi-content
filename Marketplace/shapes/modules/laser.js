b3790dec4ee2fb644e9e5d09e80056dc55c919bb
David Rowe <david@ctrlaltstudio.com>
2018-05-18 16:17:33 +1200

    Fix laser target size flash when distance suddenly reduces

--- Marketplace/shapes/modules/laser.js
+++ Marketplace/shapes/modules/laser.js
@@ -41,7 +41,6 @@ Laser = function (side) {
 
         laserLength,
         specifiedLaserLength = null,
-        laserSphereSize = 0,
 
         LEFT_HAND = 0,
 
@@ -110,16 +109,12 @@ Laser = function (side) {
         } else {
             Overlays.editOverlay(laserLine, { visible: false });
         }
-        // Avoid flash from large laser sphere when turn on or suddenly increase distance. Rendering seems to update overlay 
-        // position one frame behind so use sphere size from preceding frame.
-        updateSphere(searchTarget, laserSphereSize, color);
-        laserSphereSize = sphereSize;
+        updateSphere(searchTarget, sphereSize, color);
     }
 
     function hide() {
         Overlays.editOverlay(laserLine, { visible: false });
         Overlays.editOverlay(laserSphere, { visible: false });
-        laserSphereSize = 0;
     }
 
     function setUIOverlays(overlayIDs) {
