414f7edb2a5799d15debd451b84376406d686fee
RebeccaStankus <Rebecca@highfidelity.io>
2018-05-22 12:27:38 -0700

    Modified raypick for laser

--- Marketplace/Laser Pointer/laser.js
+++ Marketplace/Laser Pointer/laser.js
@@ -9,61 +9,74 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 /* globals RayPick */
 
-(function() { 
+(function() {
+
+    var _this;
+
     var TRIGGER_CONTROLS = [Controller.Standard.LT, Controller.Standard.RT];
     var BEAM_LOCAL_OFFSET = {x:0, y:-0.0055, z:-0.045};
     var BEAM_LOCAL_DIRECTION = {x:0, y:0, z:-1000};
     var BEAM_MIN_SIZE = 0.02;
     var TRIGGER_TOGGLE_VALUE = 0.97;
-    var RED = { red:255, green:0, blue:0 };
-    var BEAM_ORIGIN_SIZE = { x: 0.016 , y: 0.016, z: 0.016 };
-    var BEAM_ORIGIN_LOCAL_POSITION = { x: 0.00010538101196289062, y: -0.0016655921936035156, z: -0.049716949462890625};
+    var SEARCH_RADIUS = 100;
+    var ORIGIN_SIZE_RATIO = 0.53;
+    var BEAM_OFFSET_RATIO = 0.4357;
 
-    var _this;
     var equipped = false;
     var currentHand = null;
     var on = false;
-    var beamFocus;
-    /* var beamOverlay;
-    var beam;
-    var laserLine;*/
-    var beamOrigin;
+    var doNotRayPick = [];
     var triggerReleased = true;
     
-    Laser = function() {
+    var Laser = function() {
         _this = this;
     };
 
     Laser.prototype = {
+        position: null,
+        diameter: null,
+        beamOffset: null,
+        origin: null,
+        beam: null,
+        focus: null,
+
+        preload: function(entityID) {
+            _this.entityID = entityID;
+            var dimensions = Entities.getEntityProperties(_this.entityID, 'dimensions').dimensions;
+            _this.diameter = dimensions.x;
+            _this.beamOffset = { x: 0, y: 0, z: -(BEAM_OFFSET_RATIO * dimensions.z) };
+        },
+
         startEquip: function(id, params) {
+            print("start equip");
             equipped = true;
             currentHand = params[0] === "left" ? 0 : 1;
         },
 
         continueEquip: function(id, params) {
             if (!equipped) {
-                return;
+                var parentJointIndex = Entities.getEntityProperties(_this.entityID, 'parentJointIndex').parentJointIndex;
+                currentHand = (parentJointIndex === MyAvatar.getJointIndex("LeftHand")) ? 0 : 1;
+                equipped = true;
+            } else {
+                _this.toggleWithTriggerPressure();
             }
-            this.toggleWithTriggerPressure();
         },
         
         toggleWithTriggerPressure: function() {
             var triggerValue = Controller.getValue(TRIGGER_CONTROLS[currentHand]);
             if (on) {
                 if (triggerValue >= TRIGGER_TOGGLE_VALUE && triggerReleased) {
-                    // print("off");
-                    this.turnOff();
+                    _this.turnOff();
                     triggerReleased = false;
                 } else if (triggerValue < TRIGGER_TOGGLE_VALUE && !triggerReleased) {
                     triggerReleased = true;
                 } else {
-                    // print("update");
-                    this.turnOn(); // this will also update position of beam
+                    _this.turnOn();
                 }
             } else {
                 if (triggerValue >= TRIGGER_TOGGLE_VALUE && triggerReleased) {
-                    // print("on");
-                    this.turnOn();
+                    _this.turnOn();
                     triggerReleased = false;
                 } else if (triggerValue < TRIGGER_TOGGLE_VALUE && !triggerReleased) {
                     triggerReleased = true;
@@ -72,128 +85,132 @@
         },
 
         releaseEquip: function(id, params) {
+            print("release");
             currentHand = null;
             equipped = false;
-            this.turnOff();
+            _this.turnOff();
         },
         
         getBeamPosition: function() {
-            var properties = Entities.getEntityProperties(this.entityID, ['position', 'rotation']);
+            var properties = Entities.getEntityProperties(_this.entityID, ['position', 'rotation']);
             var beamLocalPosition = Vec3.multiplyQbyV(properties.rotation, BEAM_LOCAL_OFFSET);
             var beamWorldPosition = Vec3.sum(properties.position, beamLocalPosition);
             return beamWorldPosition;
         },
 
         getBeamDirection: function() {
-            var rotation = Entities.getEntityProperties(this.entityID, ['rotation']).rotation;
+            var rotation = Entities.getEntityProperties(_this.entityID, ['rotation']).rotation;
             var beamAdjustedDirection = Vec3.multiplyQbyV(rotation, BEAM_LOCAL_DIRECTION);
             return beamAdjustedDirection;
         },
 
         turnOn: function() {
-            var beamStart = this.getBeamPosition();
-            var beamDirection = this.getBeamDirection();
-            var beamEnd = Vec3.sum(beamStart, beamDirection);
+            var PICK_MAX_DISTANCE = 100;
+            
+            var beamStart = _this.getBeamPosition();
+            var beamDirection = _this.getBeamDirection();
+            var beamDirectionLength = Vec3.length(beamDirection);
+            var beamDirectionNormalized = Vec3.normalize(beamDirection);
             var beamPickRay = ({
                 origin: beamStart,
-                direction: beamEnd
+                direction: beamDirectionNormalized
             });
-            if (!on) {
-                beamOrigin = Entities.addEntity({
-                    type: "Model",
-                    modelURL: "https://hifi-content.s3.amazonaws.com/rebecca/laser/beam.fbx",
-                    dimensions: BEAM_ORIGIN_SIZE,
-                    color: RED,
-                    name: "Laser Beam",
-                    collisionless: true,
-                    parentID: _this.entityID,
-                    localPosition: BEAM_ORIGIN_LOCAL_POSITION
-                });
+            
+            var entityIntersection = Entities.findRayIntersection(beamPickRay, true, [], doNotRayPick);
+            var entityIntersectionDistance = entityIntersection.intersects ? entityIntersection.distance : Number.MAX_VALUE;
+            var avatarIntersection = AvatarList.findRayIntersection(beamPickRay);
+            var avatarIntersectionDistance = avatarIntersection.intersects ? avatarIntersection.distance : Number.MAX_VALUE;
+            var intersection = null;
+            var intersectEntityID = null;
+            if (entityIntersection.intersects && entityIntersectionDistance < avatarIntersectionDistance && 
+                entityIntersectionDistance < beamDirectionLength) {
+                intersectEntityID = entityIntersection.entityID;
+                intersection = entityIntersection;
+            } else if (avatarIntersection.intersects && avatarIntersectionDistance < entityIntersectionDistance && 
+                avatarIntersectionDistance < beamDirectionLength) {
+                intersectEntityID = avatarIntersection.avatarID;
+                intersection = avatarIntersection;
             }
-            /* var beamPickRay = RayPick.addPick({
-                origin: beamStart,
-                direction: beamEnd
-            });*/
-            // var intersection = RayPick.getEntityIntersection(beamPickRay);
-            var intersection = Entities.findRayIntersection(beamPickRay, true, [], [_this.entityID], true, false);
-            if (intersection.intersects) {
+            if (intersectEntityID) {
                 var intersectionPosition = intersection.intersection;
                 var beamPointDistance = Vec3.distance(intersectionPosition, beamStart);
-                // print("beamPointDistance is " + beamPointDistance);
-                // var beamDirectionNormalized = Vec3.normalize(beamDirection);
-                // var direction = Vec3.multiply(beamDirectionNormalized, beamPointDistance);
                 var beamSize = 0.01 * beamPointDistance;
-                // print("beamSize is " + beamSize);
-                // var newBeamPosition = Vec3.sum(beamStart, direction);
                 if (beamSize < BEAM_MIN_SIZE) {
                     beamSize = BEAM_MIN_SIZE;
                 }
-                // print("beamSize is " + beamSize);
-                if (!beamFocus) {
-                    beamFocus = Entities.addEntity({
-                        type: "Model",
-                        modelURL: "https://hifi-content.s3.amazonaws.com/rebecca/laser/beam.fbx",
-                        position: intersection.intersection,
-                        dimensions: { x: beamSize , y: beamSize, z: beamSize },
-                        color: RED,
-                        name: "Laser Beam",
+                if (!on) {
+                    print("turn on");
+                    _this.focus = Entities.addEntity({
+                        type: 'Sphere',
+                        dimensions: {x: beamSize, y: beamSize, z: beamSize},
+                        name: "Laser Focus",
+                        color: {red: 255, green: 0, blue: 0},
+                        position: intersectionPosition,
                         collisionless: true
-                    });
-                    /* print("adding beam");
-                    beam = Entities.addEntity({
-                        type: "Line",
-                        name: "normalDisplay",
-                        linePoints: [beamStart, intersectionPosition],
-                        color: { red: 255, green: 0, blue: 0 }
-                    });*/
-                    /* laserLine = Overlays.addOverlay("line3d", {
-                        type: "line3d",
-                        color: { red:0, green:255, blue:0 },
-                        visible: true,
-                        alpha: 1,
-                        solid: true,
+                    }, true);
+                    doNotRayPick.push(_this.focus);
+    
+                    var originDiameter = ORIGIN_SIZE_RATIO * _this.diameter;
+                    _this.origin = Entities.addEntity({
+                        type: 'Sphere',
+                        name: "Laser Origin",
+                        parentID: _this.entityID,
+                        localPosition: _this.beamOffset,
+                        dimensions: {x: originDiameter, y: originDiameter, z: originDiameter},
+                        color: {red: 255, green: 0, blue: 0},
+                        collisionless: true
+                    }, true);
+                    doNotRayPick.push(_this.origin);
+        
+                    _this.beam = Entities.addEntity({
+                        lifetime: 360,
+                        type: 'Line',
+                        name: "Laser Beam",
+                        canCastShadow: false,
                         glow: 1.0,
-                        ignoreRayIntersection: true, // always ignore this
-                        drawInFront: 1, // Even when burried inside of something, show it.
-                        start: beamStart,
-                        end: intersectionPosition
-                    });*/
+                        lineWidth: 5,
+                        alpha: 0.5,
+                        ignoreRayIntersection: true,
+                        drawInFront: true,
+                        color: {red: 255, green: 0, blue: 0},
+                        parentID: _this.origin,
+                        localPosition: {x: 0, y: 0, z: _this.beamOffset},
+                        localRotation: Quat.normalize({}),
+                        dimensions: Vec3.multiply(PICK_MAX_DISTANCE * 2, Vec3.ONE),
+                        linePoints: [Vec3.ZERO, {x: 0, y: 0, z: -beamPointDistance}]
+                    }, true);
+                    doNotRayPick.push(_this.beam);
                 } else {
-                    Entities.editEntity(beamFocus, {
-                        position: intersection.intersection,
-                        dimensions: { x: beamSize , y: beamSize, z: beamSize }
+                    Entities.editEntity(_this.focus, {
+                        dimensions: {x: beamSize, y: beamSize, z: beamSize},
+                        position: intersectionPosition
                     });
-                    /* Entities.editEntity(beam, {
-                        linePoints: [beamStart, intersectionPosition]
+                    Entities.editEntity(_this.beam, {
+                        localPosition: {x: 0, y: this.beamOffset, z: 0},
+                        localRotation: Quat.normalize({}),
+                        dimensions: Vec3.multiply(PICK_MAX_DISTANCE * 2, Vec3.ONE),
+                        linePoints: [Vec3.ZERO, {x: 0, y: 0, z: -beamPointDistance}]
                     });
-                    Overlays.editOverlay(laserLine, {
-                        start: beamStart,
-                        end: intersectionPosition
-                    });*/
                 }
+            } else {
+                print("no intersection");
             }
             on = true;
         },
         
         turnOff : function() {
-            if (beamFocus !== undefined) {
-                Entities.deleteEntity(beamFocus);
-                Entities.deleteEntity(beamOrigin);
-                /* Entities.deleteEntity(beam);
-                Overlays.deleteOverlay(laserLine);*/
-                beamFocus = undefined;
-                // beam = undefined;
-                beamOrigin = undefined;
-            }
+            print("off");
+            Entities.deleteEntity(_this.focus);
+            Entities.deleteEntity(_this.beam);
+            Entities.deleteEntity(_this.origin);
+            _this.focus = null;
+            _this.beam = null;
+            _this.origin = null;
             on = false;
         },
 
         unload: function() {
-            this.turnOff();
-        },
-
-        preload: function(entityID) {
-            this.entityID = entityID;          
+            _this.turnOff();
         }
     };
 
