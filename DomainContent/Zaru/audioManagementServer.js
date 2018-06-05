eeb3ad7cafba6b578301717110730e50610fcef1
Liv Erickson <liv@highfidelity.io>
2018-06-05 15:40:01 -0700

    use rotation instead of position calculation for applause

--- DomainContent/Zaru/audioManagementServer.js
+++ DomainContent/Zaru/audioManagementServer.js
@@ -1,6 +1,14 @@
+//
+//  Created by Liv Erickson on 6/4/18
+//  Copyright 2018 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
 (function(){
 
     var GROUP_CLAP_URL = Script.resolvePath("sounds/small-clap.wav");
+    var CHIME_SOUND = Script.resolvePath("sounds/bell-chime-alert.wav");
     var groupClapSound;
 
     var applauseQueue = [];
@@ -8,7 +16,7 @@
     var ApplauseManager = function() {};
 
     ApplauseManager.prototype = {
-        remotelyCallable: ["queueApplauseIntent"],
+        remotelyCallable: ["queueApplauseIntent", "playIndividualClap"],
         preload: function(entityID) {
             groupClapSound = SoundCache.getSound(GROUP_CLAP_URL);
         },
@@ -21,6 +29,42 @@
                 }
             }
             return found;
+        },
+        playIndividualClap: function(entityID, params) {
+            var chime = SoundCache.getSound(CHIME_SOUND);
+            if (chime.downloaded) {
+                Audio.playSound( chime, {
+                    position: JSON.parse(params[0]),
+                    volume: 0.5
+                });
+            }
+            var properties = { 
+                type: "ParticleEffect",
+                position: JSON.parse(params[0]),
+                isEmitting:true,
+                lifespan:2,
+                maxParticles:1,
+                textures:"https://hifi-content.s3-us-west-1.amazonaws.com/liv/Production/Rust/heart.png",
+                emitRate:1,
+                emitSpeed:0,
+                emitDimensions:{x:0,y:0,z:0},
+                particleRadius:0.1,
+                radiusSpread:0.25,
+                radiusStart:0,
+                radiusFinish:0,
+                color:{red:15,blue:135,green:14},
+                colorSpread:{red:0,blue:0,green:0},
+                colorStart:{red:235,blue:173,green:2},
+                colorFinish:{red:179,blue:30,green:0},
+                emitAcceleration:{x:-0.5,y:2.5,z:-0.5},
+                accelerationSpread:{x:0.5,y:1,z:0.5},
+                alpha:0.5,
+                alphaSpread:0.5,
+                alphaStart:0.5,
+                alphaFinish:0,
+                lifetime: 5
+            };
+            Entities.addEntity(properties);
         },
         queueApplauseIntent : function(entityID, params) {
             var _remoteCallerID = Script.remoteCallerID + "";
