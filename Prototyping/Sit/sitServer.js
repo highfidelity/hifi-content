4b0445674c8abadc378e1867cbfaa6711ca0d607
Robin Wilson <robin@ad.highfidelity.io>
2018-06-19 09:20:37 -0700

    added server sit script

--- Prototyping/Sit/sitServer.js
+++ Prototyping/Sit/sitServer.js
@@ -1,7 +1,10 @@
+/* globals Entities, Script, AnimationCache, Settings, MyAvatar, DriveKeys, AvatarList,
+ Vec3, HMD, Overlays, Camera, isInEditMode */
+
 (function() {
 
-    var isOccupied = false;
-
+    var isOccupied;
+    var entityID = null;
     function SitServer() {
 
     }
@@ -13,18 +16,27 @@
             "onStandUp"
         ],
         preload: function (id) {
-
+            entityID = id;
+            isOccupied = false;
         },
 
-        getOccupiedStatus: function () {
-            return isOccupied;
+        getOccupiedStatus: function (id, param) {
+            var clientSessionID = param[0];
+            Entities.callEntityClientMethod(
+                clientSessionID, 
+                entityID, 
+                "setIsOccupied", 
+                [JSON.stringify(isOccupied)]
+            );
         },
 
         onSitDown: function () {
+            print("hello2", isOccupied);
             isOccupied = true;
         },
 
         onStandUp: function () {
+            print("hello3", isOccupied);
             isOccupied = false;
         }
     };
