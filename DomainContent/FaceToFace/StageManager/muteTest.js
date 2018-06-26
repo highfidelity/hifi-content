d75662ad15d29f14776c293f127c3a337fa59447
RebeccaStankus <Rebecca@highfidelity.io>
2018-06-26 15:40:21 -0700

    removed prints, added EOF lines

--- DomainContent/FaceToFace/StageManager/muteTest.js
+++ DomainContent/FaceToFace/StageManager/muteTest.js
@@ -1,4 +1,14 @@
+//
+//  muteTest.js
+//
+//  Created by Rebecca Stankus on 06/21/18
+//  Copyright 2018 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+
 /* global AccountServices */
+
 (function() {
 
     var ignore;
