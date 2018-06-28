f6b3a43ea595d1c491cf36612e95a7b3f0321de7
Robin Wilson <robin@AD.HIGHFIDELITY.IO>
2018-06-28 10:14:57 -0700

    Drum listeners are positioned properly

--- Prototyping/Domains/Korea/Instruments/Janggu/Stick/createStick.js
+++ Prototyping/Domains/Korea/Instruments/Janggu/Stick/createStick.js
@@ -2,7 +2,7 @@
 
 (function() {
     var _this;
-    var STICK_CLIENT_SCRIPT_URL = Script.resolvePath("stick.js?v1" + Math.random());
+    // var STICK_CLIENT_SCRIPT_URL = Script.resolvePath("stick.js?v1" + Math.random());
 
     var JangguStick = function() {
         _this = this;
@@ -27,15 +27,9 @@
 
         makeListener: function () {
 
-            var joint = Entities.getJointIndex(_this.entityID, "joint1");
-
-            // var jointPos = Entities.getLocalJointTranslation(_this.entityID, joint);
-            
             var props = Entities.getEntityProperties(_this.entityID, ["rotation", "position", "dimensions"]);
             var dimensions = props.dimensions;
-            
-            // var scalar = Vec3.dot({x: 0, y: jangguProps.dimensions.y, z:0}, {x: 0, y: topJointPos.y, z:0});
-            
+
             var yHeight = dimensions.y / 10;
 
             var listenerProps = {
@@ -60,19 +54,14 @@
                     y: yHeight,
                     z: dimensions.z
                 },
-                position: {
-                    x: props.position.x,
-                    y: props.position.y - (dimensions.y/2 - yHeight/2),
-                    z: props.position.z
-                },
+                position: Vec3.sum(props.position, Vec3.multiplyQbyV(props.rotation, { x: 0, y: -(dimensions.y/2 - yHeight/2), z: 0 })),
                 color: { red: 255, green: 0, blue: 0 },
                 parentID: _this.entityID,
-                parentJointIndex: joint,
                 restitution: 0,
                 friction: 0,
                 collisionless: false,
                 collidesWith: "static,dynamic,kinematic,myAvatar,otherAvatar,",
-                script: STICK_CLIENT_SCRIPT_URL
+                // script: STICK_CLIENT_SCRIPT_URL
             };
             
             this.listener = Entities.addEntity(listenerProps);
