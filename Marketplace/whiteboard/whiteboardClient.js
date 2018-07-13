47331933e8f7a8b9a4f6a541741c10dcc4d05b32
Daniela Fontes <danielafilipa@gmail.com>
2018-07-13 14:57:18 +0100

    Add Feedback Overlay for eraser and hmd/desktop switch function.

--- Marketplace/whiteboard/whiteboardClient.js
+++ Marketplace/whiteboard/whiteboardClient.js
@@ -10,16 +10,27 @@
         _this.whiteboard = null;
     };
 
-    var overlayTutorialCard1On;
-    var overlayTutorialCard1Off;
+    var overlayTutorialMarker;
+    var overlayTutorialMarkerArrow;
+
+    var overlayTutorialEraserArrow;
+    var overlayTutorialEraser;
 
     var MARKER_TUTORIAL_OFFSET = {x: -0.8, y: 0.15, z: -0.15};
-    var MARKER_ARROW_TUTORIAL_OFFSET = {x: -0.52, y: 0.15, z: -0.15};
+    var MARKER_ARROW_TUTORIAL_OFFSET = {x: -0.52, y: 0.15, z: -0.2};
 
     var MARKER_TUTORIAL_DIMENSIONS = {x: 1.5, y: 1.5};
     var MARKER_ARROW_TUTORIAL_DIMENSIONS = {x: 0.5, y: 0.5};
 
     var TUTORIAL_OVERLAY_RADIUS = 5.0;
+
+    var MARKER_TUTORIAL_DESKTOP_URL = "tutorial/Whiteboard-desktopmode-instructions-markers.png";
+    var MARKER_TUTORIAL_HMD_URL = "tutorial/Whiteboard-VRmode-instructions-markers.png";
+    var ERASER_TUTORIAL_DESKTOP_URL = "tutorial/Whiteboard-VRmode-instructions-eraser.png";
+    var ERASER_TUTORIAL_HMD_URL = "tutorial/Whiteboard-VRmode-instructions-eraser.png";
+    var ARROW_TUTORIAL_URL = "tutorial/Whiteboard-instructions-arrow.png";
+
+
 
     WhiteboardClient.prototype = {
         preload: function(entityID) {
@@ -59,9 +70,31 @@
                 )
             );
 
-            overlayTutorialCard1On = Overlays.addOverlay("image3d", {
-                url: Script.resolvePath("tutorial/Whiteboard-desktopmode-instructions-markers.png"),
+            overlayTutorialMarker = Overlays.addOverlay("image3d", {
+                url: Script.resolvePath(HMD.active ? MARKER_TUTORIAL_HMD_URL : MARKER_TUTORIAL_DESKTOP_URL),
                 position: markerOverlayPosition,
+                rotation: whiteboardRotation,
+                dimensions: MARKER_TUTORIAL_DIMENSIONS,
+                name: "Tutorial Marker Desktop",
+                isFacingAvatar: false
+            });
+            var overlayTutorialEraserPosition = Vec3.sum(
+                markerOverlayPosition, 
+                Vec3.multiply(
+                    -2*whiteboardDimensions.x * MARKER_TUTORIAL_OFFSET.x, 
+                    whiteboardRight
+                )
+            );
+            overlayTutorialEraserPosition = Vec3.sum(
+                overlayTutorialEraserPosition, 
+                Vec3.multiply(
+                    -1.5*whiteboardDimensions.y * MARKER_TUTORIAL_OFFSET.y, 
+                    whiteboardUp
+                )
+            );
+            overlayTutorialEraser = Overlays.addOverlay("image3d", {
+                url: Script.resolvePath(HMD.active ? ERASER_TUTORIAL_HMD_URL : ERASER_TUTORIAL_DESKTOP_URL),
+                position: overlayTutorialEraserPosition,
                 rotation: whiteboardRotation,
                 dimensions: MARKER_TUTORIAL_DIMENSIONS,
                 name: "Tutorial Marker Desktop",
@@ -90,8 +123,8 @@
                 )
             );
 
-            overlayTutorialCard1Off = Overlays.addOverlay("image3d", {
-                url: Script.resolvePath("tutorial/Whiteboard-instructions-arrow.png"),
+            overlayTutorialMarkerArrow = Overlays.addOverlay("image3d", {
+                url: Script.resolvePath(ARROW_TUTORIAL_URL),
                 position: markerArrowOverlayPosition,
                 rotation: whiteboardRotation,
                 dimensions: MARKER_ARROW_TUTORIAL_DIMENSIONS,
@@ -99,6 +132,51 @@
                 isFacingAvatar: false,
                 visible: true
             });
+            var eraserArrowOverlayPosition = Vec3.sum(
+                whiteboardPosition,
+                Vec3.multiply(
+                    -1.0 * whiteboardDimensions.x * MARKER_ARROW_TUTORIAL_OFFSET.x, 
+                    whiteboardRight
+                )
+            );
+            eraserArrowOverlayPosition = Vec3.sum(
+                eraserArrowOverlayPosition,
+                Vec3.multiply(
+                    -1.0 * whiteboardDimensions.y * MARKER_ARROW_TUTORIAL_OFFSET.y, 
+                    whiteboardUp
+                )
+            );
+            eraserArrowOverlayPosition = Vec3.sum(
+                eraserArrowOverlayPosition,
+                Vec3.multiply(
+                    whiteboardDimensions.z * MARKER_ARROW_TUTORIAL_OFFSET.z, 
+                    whiteboardFront
+                )
+            );
+            overlayTutorialEraserArrow = Overlays.addOverlay("image3d", {
+                url: Script.resolvePath(ARROW_TUTORIAL_URL),
+                position: eraserArrowOverlayPosition,
+                rotation: whiteboardRotation,
+                localRotation: Quat.fromVec3Degrees({ x: 0, y: 0, z: 180 }),
+                dimensions: MARKER_ARROW_TUTORIAL_DIMENSIONS,
+                name: "Tutorial Marker Desktop Arrow",
+                isFacingAvatar: false,
+                visible: true
+            });
+
+            HMD.displayModeChanged.connect(_this.onHmdChanged);
+        },
+        onHmdChanged: function(isHMDActive) {
+            // change marker instructions overlay
+            Overlays.editOverlay(overlayTutorialMarker, {
+                url: Script.resolvePath(isHMDActive ? MARKER_TUTORIAL_HMD_URL : MARKER_TUTORIAL_DESKTOP_URL)
+            });
+
+            // change eraser instructions overlay
+            Overlays.editOverlay(overlayTutorialEraser, {
+                url: Script.resolvePath(isHMDActive ? ERASER_TUTORIAL_HMD_URL : ERASER_TUTORIAL_DESKTOP_URL)
+            });
+                      
         },        
         clearBoard: function() {
                       
@@ -126,8 +204,9 @@
             
         },
         unload: function() {
-            Overlays.deleteOverlay(overlayTutorialCard1Off);
-            Overlays.deleteOverlay(overlayTutorialCard1On);
+            Overlays.deleteOverlay(overlayTutorialMarkerArrow);
+            Overlays.deleteOverlay(overlayTutorialMarker);
+            HMD.displayModeChanged.disconnect(_this.onHmdChanged);
         }
     };
 
