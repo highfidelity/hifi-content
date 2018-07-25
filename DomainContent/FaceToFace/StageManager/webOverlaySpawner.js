d530e06d2ed5361c30216dbe0c8f579df837c742
Liv Erickson <liv@highfidelity.io>
2018-07-25 11:25:38 -0700

    code cleanup per CR

--- DomainContent/FaceToFace/StageManager/webOverlaySpawner.js
+++ DomainContent/FaceToFace/StageManager/webOverlaySpawner.js
@@ -5,8 +5,8 @@
     var OVERLAY_PROPERTIES_BASE = {
         maxFPS: FPS,
         locked: true,
-        dimensions: {x: 1.6, y: 0.9, z: 0.01},
-        rotation: Quat.fromVec3Degrees({x: 0, y: -109, z: 0}),
+        dimensions: {x: 11.6894, y: 4.9342, z: 0.01},
+        rotation: Quat.fromVec3Degrees({x: 0, y: -3, z: 0}),
         alpha: 1.0
     };
 
@@ -15,7 +15,7 @@
 
     function createOverlay() {
         var overlayProperties = OVERLAY_PROPERTIES_BASE;
-        overlayProperties.position = {x: -0.6, y: -10.2, z: -1.9};
+        overlayProperties.position = {x: 98.5507, y: 6.56, z: 23.7452};
         overlayProperties.parentID = _entityID;
         overlayProperties.url = _url; 
         overlay = Overlays.addOverlay('web3d', overlayProperties);
@@ -26,8 +26,14 @@
     WebOverlaySpawner.prototype = {
         preload: function(entityID) {
             _entityID = entityID;
-            _url = JSON.parse(Entities.getEntityProperties(entityID, 'userData').userData).url;
-            createOverlay();
+            try {
+                _url = JSON.parse(Entities.getEntityProperties(entityID, 'userData').userData).url;
+                createOverlay();
+            } catch (error) {
+                print("No URL is available to create the overlay, will not create. Deleting spawner.");
+                Entities.deleteEntity(entityID);
+            }
+
         },
         unload: function() {
             Overlays.editOverlay(overlay, {'url': 'https://highfidelity.com'});
