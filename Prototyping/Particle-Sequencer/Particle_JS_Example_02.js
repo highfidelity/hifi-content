7b4c4442549e9d31d137114d9a138bf722197119
milad nazeri <milad@miladnazeri.com>
2018-09-07 01:24:08 -0700

    Added primary default particles

--- Prototyping/Particle-Sequencer/Particle_JS_Example_02.js
+++ Prototyping/Particle-Sequencer/Particle_JS_Example_02.js
@@ -158,14 +158,206 @@ var textures = {
 
 // Add your particles here
 var sequencedEntities = {
-    "FIRE": 
-        {"isEmitting":true,"lifespan":"1.5","maxParticles":"10","textures":"https://content.highfidelity.com/DomainContent/production/Particles/wispy-smoke.png","emitRate":"5.5","emitSpeed":"0","speedSpread":"0","emitDimensions":{"x":"0","y":"0","z":"0"},"emitOrientation":{"x":"-90","y":"0","z":"0"},"emitterShouldTrail":true,"particleRadius":"0.25","radiusSpread":"0","radiusStart":"0","radiusFinish":"0.10000000149011612","color":{"red":"200","blue":"200","green":"200"},"colorSpread":{"red":"0","blue":"0","green":"0"},"colorStart":{"red":"200","blue":"200","green":"200"},"colorFinish":{"red":"0","blue":"0","green":"0"},"emitAcceleration":{"x":"-0.5","y":"2.5","z":"-0.5"},"accelerationSpread":{"x":"0.5","y":"1","z":"0.5"},"alpha":"0","alphaSpread":"0","alphaStart":"1","alphaFinish":"0","particleSpin":0,"spinSpread":0,"spinStart":0,"spinFinish":0,"rotateWithEntity":false,"polarStart":0,"polarFinish":0,"azimuthStart":-3.1415927410125732,"azimuthFinish":3.1415927410125732}  
-    ,
-    "SMOKE": 
-        { "isEmitting": true, "lifespan": "1.5", "maxParticles": "10", "textures": "https://content.highfidelity.com/DomainContent/production/Particles/wispy-smoke.png", "emitRate": "95", "emitSpeed": "1.35", "speedSpread": "1.35", "emitDimensions": { "x": "0", "y": "20", "z": "0" }, "emitOrientation": { "x": "0", "y": "90", "z": "0" }, "emitterShouldTrail": true, "particleRadius": "0.25", "radiusSpread": "0", "radiusStart": "0", "radiusFinish": "0.10000000149011612", "color": { "red": "200", "blue": "200", "green": "200" }, "colorSpread": { "red": "0", "blue": "0", "green": "0" }, "colorStart": { "red": "200", "blue": "200", "green": "200" }, "colorFinish": { "red": "0", "blue": "0", "green": "0" }, "emitAcceleration": { "x": "-0.5", "y": "2.5", "z": "-0.5" }, "accelerationSpread": { "x": "0.5", "y": "1", "z": "0.5" }, "alpha": "0", "alphaSpread": "0", "alphaStart": "1", "alphaFinish": "0", "particleSpin": 0, "spinSpread": 0, "spinStart": 0, "spinFinish": 0, "rotateWithEntity": false, "polarStart": 0, "polarFinish": 0, "azimuthStart": -3.1415927410125732, "azimuthFinish": 3.1415927410125732 }
-    ,
-    "SUN": 
-        { "isEmitting": true, "lifespan": "1.5", "maxParticles": "10", "textures": "https://content.highfidelity.com/DomainContent/production/Particles/wispy-smoke.png", "emitRate": "95", "emitSpeed": "1.35", "speedSpread": "1.35", "emitDimensions": { "x": "0", "y": "20", "z": "0" }, "emitOrientation": { "x": "0", "y": "90", "z": "0" }, "emitterShouldTrail": true, "particleRadius": "0.25", "radiusSpread": "0", "radiusStart": "0", "radiusFinish": "0.10000000149011612", "color": { "red": "200", "blue": "200", "green": "200" }, "colorSpread": { "red": "0", "blue": "0", "green": "0" }, "colorStart": { "red": "200", "blue": "200", "green": "200" }, "colorFinish": { "red": "0", "blue": "0", "green": "0" }, "emitAcceleration": { "x": "-0.5", "y": "2.5", "z": "-0.5" }, "accelerationSpread": { "x": "0.5", "y": "1", "z": "0.5" }, "alpha": "0", "alphaSpread": "0", "alphaStart": "1", "alphaFinish": "0", "particleSpin": 0, "spinSpread": 0, "spinStart": 0, "spinFinish": 0, "rotateWithEntity": false, "polarStart": 0, "polarFinish": 0, "azimuthStart": -3.1415927410125732, "azimuthFinish": 3.1415927410125732 }
+        "SMOKE": {
+            "accelerationSpread": {
+            "blue": 0.20000000298023224,
+            "green": 0.20000000298023224,
+            "red": 0.20000000298023224,
+            "x": 0.20000000298023224,
+            "y": 0.20000000298023224,
+            "z": 0.20000000298023224
+        },
+        "alpha": 0,
+        "alphaFinish": 0,
+        "alphaStart": 1,
+        "color": {
+            "blue": 200,
+            "green": 200,
+            "red": 200
+        },
+        "colorFinish": {
+            "blue": 0,
+            "green": 0,
+            "red": 0,
+            "x": 0,
+            "y": 0,
+            "z": 0
+        },
+        "colorStart": {
+            "blue": 61,
+            "green": 61,
+            "red": 61,
+            "x": 61,
+            "y": 61,
+            "z": 61
+        },
+        "emitAcceleration": {
+            "blue": 0,
+            "green": 1,
+            "red": 0,
+            "x": 0,
+            "y": 1,
+            "z": 0
+        },
+        "emitOrientation": {
+            "w": 0.7070763111114502,
+            "x": -0.7071372866630554,
+            "y": -1.5258539860951714e-05,
+            "z": -1.5258539860951714e-05
+        },
+        "emitRate": 30,
+        "emitSpeed": 0,
+        "maxParticles": 40,
+        "particleRadius": 2,
+        "radiusFinish": 0,
+        "radiusStart": 1,
+        "speedSpread": 0,
+        "spinFinish": null,
+        "spinStart": null,
+        "textures": "http://hifi-content.s3.amazonaws.com/alexia/Particles/smoke.png"
+    },
+    "TWINKLE": {
+        "accelerationSpread": {
+            "blue": 0.5,
+            "green": 1,
+            "red": 0.5,
+            "x": 0.5,
+            "y": 1,
+            "z": 0.5
+        },
+        "alphaFinish": 1,
+        "alphaSpread": 1,
+        "alphaStart": 1,
+        "clientOnly": false,
+        "color": {
+            "blue": 0,
+            "green": 183,
+            "red": 255
+        },
+        "colorFinish": {
+            "blue": 255,
+            "green": 252,
+            "red": 255,
+            "x": 255,
+            "y": 252,
+            "z": 255
+        },
+        "colorStart": {
+            "blue": 20,
+            "green": 149,
+            "red": 255,
+            "x": 255,
+            "y": 149,
+            "z": 20
+        },
+        "emitAcceleration": {
+            "blue": 0,
+            "green": 0,
+            "red": 0,
+            "x": 0,
+            "y": 0,
+            "z": 0
+        },
+        "emitDimensions": {
+            "blue": 0.5,
+            "green": 0.5,
+            "red": 0.5,
+            "x": 0.5,
+            "y": 0.5,
+            "z": 0.5
+        },
+        "emitOrientation": {
+            "w": 0.7070915699005127,
+            "x": -0.7071220278739929,
+            "y": -1.5258869098033756e-05,
+            "z": -1.5258869098033756e-05
+        },
+        "emitRate": 100,
+        "emitSpeed": 0,
+        "emitterShouldTrail": true,
+        "lifespan": 0.699999988079071,
+        "maxParticles": 6,
+        "particleRadius": 0.20000000298023224,
+        "particleSpin": 0.2617993950843811,
+        "polarFinish": 3.1415927410125732,
+        "radiusFinish": 0.10000000149011612,
+        "radiusSpread": 0.30000001192092896,
+        "radiusStart": 0,
+        "speedSpread": 0.5,
+        "spinFinish": 1.3089969158172607,
+        "spinSpread": 3.0019662380218506,
+        "spinStart": -0.8901179432868958,
+        "textures": "http://hifi-content.s3.amazonaws.com/alexia/Particles/twinkle.png",
+    },
+    "HEARTS": {
+        "accelerationSpread": {
+            "blue": 0.5,
+            "green": 1,
+            "red": 0.5,
+            "x": 0.5,
+            "y": 1,
+            "z": 0.5
+        },
+        "alphaFinish": 1,
+        "alphaStart": 1,
+        "clientOnly": false,
+        "color": {
+            "blue": 0,
+            "green": 0,
+            "red": 255
+        },
+        "colorFinish": {
+            "blue": 162,
+            "green": 0,
+            "red": 255,
+            "x": 255,
+            "y": 0,
+            "z": 162
+        },
+        "colorStart": {
+            "blue": 0,
+            "green": 0,
+            "red": 255,
+            "x": 255,
+            "y": 0,
+            "z": 0
+        },
+        "emitAcceleration": {
+            "blue": -0.5,
+            "green": 2,
+            "red": -0.5,
+            "x": -0.5,
+            "y": 2,
+            "z": -0.5
+        },
+        "emitDimensions": {
+            "blue": 0.5,
+            "green": 0,
+            "red": 0.5,
+            "x": 0.5,
+            "y": 0,
+            "z": 0.5
+        },
+        "emitOrientation": {
+            "w": 1,
+            "x": -1.52587890625e-05,
+            "y": -1.52587890625e-05,
+            "z": -1.52587890625e-05
+        },
+        "emitRate": 3,
+        "emitSpeed": 0,
+        "emitterShouldTrail": true,
+        "lifespan": 1,
+        "maxParticles": 5,
+        "particleRadius": 0.25,
+        "polarFinish": 3.1415927410125732,
+        "radiusFinish": 0.10000000149011612,
+        "radiusStart": 0,
+        "speedSpread": 0,
+        "spinFinish": null,
+        "spinStart": null,
+        "textures": "http://hifi-content.s3.amazonaws.com/alexia/Particles/heart.png"
+    }
 
 };
 
@@ -174,7 +366,7 @@ var sequencedEntities = {
 
 // Add your sequence below
 var sequence = {
-    "FIRE": [
+    "SMOKE": [
         [
             "START explode",
             "CHANGE position TO 0.1,0,0 AT 100",
@@ -234,7 +426,7 @@ var sequence = {
             "END"
         ]
     ],
-    "SMOKE": [
+    "TWINKLE": [
         [
             "START explode",
             "CHANGE position TO 0.5,0,0 AT 100",
@@ -294,7 +486,7 @@ var sequence = {
             "END"
         ]
     ],
-    "SUN": [
+    "HEARTS": [
         [
             "START explode",
             "CHANGE position TO 0.1,0,0.4 AT 100",
