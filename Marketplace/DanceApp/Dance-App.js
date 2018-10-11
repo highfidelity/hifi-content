b26c9647576fee2e5121bc5727249517d4b40ce3
milad <milad@miladnazeri.com>
2018-10-11 16:01:45 -0700

    Dance App

---
+++ Marketplace/DanceApp/Dance-App.js
@@ -0,0 +1,282 @@
+// Neuroscape_Diagnostic.js
+//
+// Created by Milad Nazeri and Liv Erikson on 2018-07-16
+//
+// Copyright 2018 High Fidelity, Inc.
+//
+// Distributed under the Apache License, Version 2.0.
+// See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+
+"use strict";
+/* eslint-disable indent */
+//
+//  Dance-App
+//
+//  Created by Milad Nazeri on 2018-10-11
+//  with Mmdified Code from Howard Stearns
+//  Copyright 2016 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+/* globals Tablet, Script, HMD, Controller, Menu */
+
+(function () { // BEGIN LOCAL_SCOPE
+    // Dependencies
+    // /////////////////////////////////////////////////////////////////////////
+        var 
+            AppUi = Script.require('./AppUi.js?' + Date.now()),
+            Helper = Script.require("../../Helper.js")
+        ;
+
+    // Consts
+    // /////////////////////////////////////////////////////////////////////////
+        var 
+            URL = Script.resolvePath("./Tablet/Dance-App_Tablet.html"),
+            BUTTON_NAME = "BUTTON_NAME",
+
+            PREVIEW_DANCE = "preview_dance",
+            PREVIEW_DANCE_STOP = "preview_dance_stop",
+            STOP_DANCE = "stop_dance",
+            START_DANCING = "start_dancing",
+            TRY_DANCE = "try_dance",
+            ADD_DANCE = "add_dance",
+            REMOVE_DANCE = "remove_dance",
+            UPDATE_DANCE_ARRAY = "update_dance_array",
+            CURRENT_DANCE = "current_dance",
+            DEFAULT_DURATION = "1500",
+            DEFAULT_START_FRAME = 0,
+            EVENT_BRIDGE_OPEN_MESSAGE = "eventBridgeOpen"
+        ;
+    
+    // Init
+    // /////////////////////////////////////////////////////////////////////////
+        var 
+            rustDancing = false,
+            overlay = null,
+            ui
+        ;
+
+    // Constructor
+    // /////////////////////////////////////////////////////////////////////////
+        function DanceAnimation(name, url, frames, fps) {
+            this.name = name;
+            this.url = url;
+            this.startFrame = DEFAULT_START_FRAME;
+            this.endFrame = frames;
+            this.fps = fps;
+        }
+
+        function DanceListEntry(name, url, startFrame, endFrame, duration, fps) {
+            this.name = name;
+            this.url = url;
+            this.startFrame = startFrame;
+            this.endFrame = endFrame;
+            this.duration = duration;
+            this.fps = fps;
+        }
+    
+    // Collections
+    // /////////////////////////////////////////////////////////////////////////
+        var 
+            danceUrls = Script.require("./Dance-URLS.js?"+ Date.now()),
+            dataStore = {
+                shouldBeRunning: true,
+                danceArray: [],
+                currentIndex: 0,
+                ui: {
+                    currentDance: false,
+                    danceArray: false
+                }
+            },
+            danceObjects = []
+        ;
+    
+    // Helper Functions
+    // /////////////////////////////////////////////////////////////////////////
+        function splitDanceUrls() {
+            var regex = /(https:\/\/.*\/)([a-zA-Z0-9 ]+) (\d+)(.fbx)/;
+            danceUrls.sort(function(a,b) { 
+                if (a < b) return -1;
+                else if (a > b) return 1;
+                return 0; 
+            }).forEach(function(dance) {
+                var regMatch = regex.exec(dance);
+                danceObjects.push(
+                    new DanceAnimation(
+                        regMatch[2],
+                        dance,
+                        regMatch[3],
+                        30
+                    )
+                );
+            });
+        }
+        
+        function vec(x, y, z) {
+            var obj = {};
+            obj.x = x;
+            obj.y = y;
+            obj.z = z;
+            return obj;
+        }
+
+    // Procedural Functions
+    // /////////////////////////////////////////////////////////////////////////
+        function previewDanceAnimation(danceObj) {
+            var localOffset = vec(0,0,-2),
+                worldOffset = VEC3.multiplyQbyV(MyAvatar.orientation, localOffset),
+                modelPosition = VEC3.sum(MyAvatar.position, worldOffset);
+
+            overlay = Overlays.addOverlay("model", {
+                url: MyAvatar.skeletonModelURL,
+                position: modelPosition,
+                animationSettings: {
+                    url: danceObj.url,
+                    fps: danceObj.fps,
+                    loop: true,
+                    running: true,
+                    lastFrame: danceObj.frames
+                }
+            });
+        }
+
+        function stopPreviewDanceAnimation() {
+            Overlays.deleteOverlay(overlay);
+        }
+
+        function addDanceAnimation(danceObj) {
+            dataStore.danceArray.push(
+                new DanceListEntry(
+                    danceObj.name,
+                    danceObj.url,
+                    danceObj.startFrame,
+                    danceObj.endFrame,
+                    DEFAULT_DURATION,
+                    danceObj.fps
+                )
+            );
+            dataStore.ui.danceArray = true;
+            ui.updateUI(dataStore);
+        }
+
+        function removeDanceAnimations(danceObj) {
+            dataStore.danceArray = [];
+            dataStore.ui.danceArray = false;
+            ui.updateUI(dataStore);
+        }
+
+        function removeDanceAnimation(index) {
+            dataStore.danceArray.splice(index,1);
+            if (dataStore.danceArray.length === 0) {
+                stopDanceAnimation();
+                dataStore.ui.danceArray = false;
+            }
+            ui.updateUI(dataStore);
+        }
+
+        function playDanceArray(){
+            dataStore.shouldBeRunning = true;
+            dataStore.currentIndex = 0;
+            playNextDance(dataStore.currentIndex);
+            ui.updateUI(dataStore);
+        }
+
+        function playNextDance(index) {
+            if ( index >= dataStore.danceArray.length) {
+                index = 0;
+            }
+            var danceArrayObject = dataStore.danceArray[index];
+            dataStore.currentIndex++;
+            dataStore.currentIndex = 
+                dataStore.currentIndex >= dataStore.danceArray.length
+                    ? 0
+                    : dataStore.currentIndex;
+
+            tryDanceAnimation(danceArrayObject);
+            Script.setTimeout(function(){
+                if (dataStore.shouldBeRunning) {
+                    playNextDance(dataStore.currentIndex);
+                }
+            }, danceArrayObject.duration);
+
+            ui.updateUI(dataStore);
+        }
+
+        function tryDanceAnimation(danceObj) {
+            rustDancing = Settings.getValue("isDancing", false);
+            MyAvatar.overrideAnimation(danceObj.url, danceObj.fps, true, danceObj.startFrame, danceObj.endFrame);
+            Settings.setValue("isDancing", true);
+            dataStore.ui.currentDance = true; 
+            dataStore.currentDance = danceObj;
+            ui.updateUI(dataStore, {slice: CURRENT_DANCE});
+        }
+
+        function stopDanceAnimation() {
+            MyAvatar.restoreAnimation();
+            Settings.setValue("isDancing", false);
+            dataStore.ui.currentDance = false; 
+            dataStore.currentDance = null;
+            dataStore.shouldBeRunning = false;
+            ui.updateUI(dataStore);
+        }
+
+        function updateDanceArray(danceArray) {
+            dataStore.danceArray = danceArray;
+            ui.updateUI(dataStore);
+        }
+
+    // Tablet
+    // /////////////////////////////////////////////////////////////////////////
+        function startup() {
+            ui = new AppUi({
+                buttonName: BUTTON_NAME,
+                sortOrder: 6,
+                home: URL,
+                onMessage: onMessage
+            });
+        }
+
+        function onMessage(data) {
+            // EventBridge message from HTML script.
+            switch (data.type) {
+                case EVENT_BRIDGE_OPEN_MESSAGE:
+                    ui.updateUI(dataStore);
+                    break;
+                case ADD_DANCE:
+                    addDanceAnimation(data.value);
+                    break;
+                case REMOVE_DANCE:
+                    removeDanceAnimation(data.value);
+                    break;
+                case START_DANCING:
+                    playDanceArray();
+                    break;
+                case TRY_DANCE:
+                    tryDanceAnimation(data.value);
+                    break;
+                case UPDATE_DANCE_ARRAY:
+                    updateDanceArray(data.value);
+                    break;
+                case STOP_DANCE:
+                    stopDanceAnimation(data.value);
+                    break;
+                case PREVIEW_DANCE:
+                    previewDanceAnimation(data.value);
+                    break;
+                case PREVIEW_DANCE_STOP:
+                    stopPreviewDanceAnimation();
+                    break;
+            }
+        }
+     
+    // Main
+    // /////////////////////////////////////////////////////////////////////////
+        dataStore.danceObjects = danceObjects;
+        splitDanceUrls();
+        startup();
+        for (var key in ui) {
+            print ("ui key: " + key);
+        }
+
+}()); // END LOCAL_SCOPE
---
+++ Marketplace/DanceApp/Tablet/js/Dance-App_Tablet.js
@@ -0,0 +1,236 @@
+(function() {
+
+    "use strict";
+
+    // Consts
+    // /////////////////////////////////////////////////////////////////////////
+        var 
+            EVENT_BRIDGE_OPEN_MESSAGE = "eventBridgeOpen",
+            UPDATE_UI = "update_ui",
+            TRY_DANCE = "try_dance",
+            STOP_DANCE = "stop_dance",
+            START_DANCING = "start_dancing",
+            REMOVE_DANCE = "remove_dance",
+            ADD_DANCE = "add_dance",
+            PREVIEW_DANCE = "preview_dance",
+            PREVIEW_DANCE_STOP = "preview_dance_stop",
+            UPDATE_DANCE_ARRAY = "update_dance_array",
+            CURRENT_DANCE = "current_dance",
+            
+            EVENTBRIDGE_SETUP_DELAY = 1000
+        ;
+
+    // Components
+    // /////////////////////////////////////////////////////////////////////////
+        Vue.component('current-dance', {
+            props: {
+                current: { type: Object}
+            },
+            methods: {
+                stopDance(){
+                    EventBridge.emitWebEvent(JSON.stringify({
+                        type: STOP_DANCE
+                    }));
+                }
+            },
+            template:`
+                <div class="card">
+                    <div class="card-header">
+                        <h4> Current Dance </h4>
+                    </div>
+                    <div class="card-body">
+                        <h4> {{ current.name }} </h4>
+                        <button class="btn-sm btn-primary mt-1 mr-1" v-on:click="stopDance()">Stop Dance</button>
+                    </div>
+                </div>
+            `
+        })
+
+        Vue.component('dance-list', {
+            props: {
+                dances: { type: Array}
+            },
+            methods: {
+                startDancing(){
+                    EventBridge.emitWebEvent(JSON.stringify({
+                        type: START_DANCING
+                    }));
+                },
+                removeDance(index){
+                    EventBridge.emitWebEvent(JSON.stringify({
+                        type: REMOVE_DANCE,
+                        value: index
+                    }));
+                },
+                onBlur(){
+                    EventBridge.emitWebEvent(JSON.stringify({
+                        type: UPDATE_DANCE_ARRAY,
+                        value: this.dances
+                    }));
+                }
+            },
+            template:`
+                <div class="accordian" id="dance-accordian">
+                    <div class="card">
+                        <div class="card-header">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne">
+                            <h4>Dance List - click to open/close</h4>
+                                
+                            </button>
+                            <br>
+                            
+                        </div>
+                        <div id="collapseOne" class="collapse show" data-parent="#dance-accordian">
+                            <div class="card-body">
+                                <draggable :dances="dances">
+                                    <transition-group name="list-complete">
+                                        <div v-for="(dance, index) in dances" 
+                                            v-bind:key="(dance.name + index)"
+                                            class="list-complete-item" 
+                                        >
+                                            <form class="form-inline">
+                                                        <h3> {{dance.name}}</h3>
+                                                        <div class="input-group mb-3 ">
+                                                            <div class="input-group-prepend">
+                                                                <span class="input-group-text">Start Frame</span>
+                                                            </div>
+                                                            <input type="text" col-sm-1 v-on:blur="onBlur" v-model="dance.startFrame" class="form-control" placeholder="start frame">
+                                                        </div>
+
+                                                        <div class="input-group mb-3">
+                                                            <div class="input-group-prepend">
+                                                                <span class="input-group-text">End Frame</span>
+                                                            </div>
+                                                            <input type="text" v-on:blur="onBlur" v-model="dance.endFrame" class="form-control" placeholder="end frame">
+                                                        </div>
+
+                                                        <div class="input-group mb-3">
+                                                            <div class="input-group-prepend">
+                                                                <span class="input-group-text">Duration</span>
+                                                            </div>
+                                                            <input type="text" v-on:blur="onBlur" v-model="dance.duration" class="form-control" placeholder="duration">
+                                                        </div>
+                                                        <div class="input-group mb-3">
+                                                            <div class="input-group-prepend">
+                                                                <span class="input-group-text">fps</span>
+                                                            </div>
+                                                            <input type="text" v-on:blur="onBlur" v-model="dance.fps" class="form-control" placeholder="fps">
+                                                        </div>
+                                                <div>
+                                                    <button class="btn-sm btn-primary mt-1 mr-1" v-on:click="removeDance(index)">Remove</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </transition-group>
+                                </draggable>
+                                <div>
+                                    <button class="btn-sm btn-primary mt-1 mr-1" v-on:click="startDancing()">start Dance</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `
+        })
+
+        Vue.component('dance', {
+            props: {
+                dance: { type: Object}
+            },
+            methods: {
+                addDance(){
+                    EventBridge.emitWebEvent(JSON.stringify({
+                        type: ADD_DANCE,
+                        value: this.dance
+                    }));
+                },
+                tryDance(){
+                    EventBridge.emitWebEvent(JSON.stringify({
+                        type: TRY_DANCE,
+                        value: this.dance
+                    }));
+                },
+                previewDance(){
+                    // console.log("preview Dance")
+                    EventBridge.emitWebEvent(JSON.stringify({
+                        type: PREVIEW_DANCE,
+                        value: this.dance
+                    }));
+                },
+                previewDanceStop(){
+                    // console.log("preview Dance Stop")
+                    EventBridge.emitWebEvent(JSON.stringify({
+                        type: PREVIEW_DANCE_STOP,
+                        value: this.dance
+                    }));
+                },
+            },
+            template:`
+                <div class="card">
+                    <div class="card-header">
+                        {{ dance.name }}
+                    </div>
+                    <div class="card-body">
+                        <button class="btn-sm btn-primary mt-1 mr-1" v-on:mouseover="previewDance()" v-on:mouseleave="previewDanceStop()" v-on:click="addDance()">Add Dance!</button>
+                    </div>
+                </div>
+            `
+        })
+
+    // App
+    // /////////////////////////////////////////////////////////////////////////
+        var app = new Vue({
+            el: '#app',
+            data: {
+                dataStore: {
+                    ui: {
+                        currentDance: false,
+                        danceList: false
+                    }
+                }
+            }
+        });
+
+    // Procedural
+    // /////////////////////////////////////////////////////////////////////////
+        function onScriptEventReceived(message) {
+            console.log(JSON.stringify(message));
+            var data;
+            try {
+                data = JSON.parse(message);
+                switch (data.type) {
+                    case UPDATE_UI:
+                        if (data.update.slice === CURRENT_DANCE) {
+                            app.dataStore.currentDance = data.value.currentDance;
+                        } else {
+                            app.dataStore = data.value;
+                        }
+                        break;
+                    default:
+                }
+            } catch (e) {
+                console.log(e)
+                return;
+            }
+        }
+        
+        function onLoad() {
+            
+            // Initial button active state is communicated via URL parameter.
+            // isActive = location.search.replace("?active=", "") === "true";
+
+            setTimeout(function () {
+                // Open the EventBridge to communicate with the main script.
+                // Allow time for EventBridge to become ready.
+                EventBridge.scriptEventReceived.connect(onScriptEventReceived);
+                EventBridge.emitWebEvent(JSON.stringify({
+                    type: EVENT_BRIDGE_OPEN_MESSAGE
+                }));
+            }, EVENTBRIDGE_SETUP_DELAY);
+        }
+
+    // Main
+    // /////////////////////////////////////////////////////////////////////////    
+        onLoad();
+
+}());
