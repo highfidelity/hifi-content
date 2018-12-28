7c10aa2646bf49ea68f00b80e51b7abf52349d7f
RebeccaStankus <Rebecca@highfidelity.io>
2018-12-27 17:04:38 -0800

    updated all scripts for blueprints

--- Marketplace/drumKit/ride.js
+++ Marketplace/drumKit/ride.js
@@ -2,6 +2,7 @@
 //  ride.js
 //
 //  created by Rebecca Stankus on 01/24/18
+//  updated 12/26/18
 //  Copyright 2018 High Fidelity, Inc.
 //
 //  Distributed under the Apache License, Version 2.0.
@@ -12,13 +13,13 @@
     var _this;
 
     var AUDIO_VOLUME_LEVEL = 0.8;
-    var HALF = 0.5;
-    var TIMEOUT_10_MS = 10;
+    var TIMEOUT_MS = 100;
+    var BOW_SOUND = SoundCache.getSound(Script.resolvePath("Assets/Sounds/bow.wav"));
+    var BELL_SOUND = SoundCache.getSound(Script.resolvePath("Assets/Sounds/bell.wav"));
+    var CRASH_SOUND = SoundCache.getSound(Script.resolvePath("Assets/Sounds/crash.wav"));
 
     var playing = false;
-    var soundEdge;
-    var soundBell;
-    var soundMid;
+    var injector;
     var topCenter;
 
     var Ride = function() {
@@ -29,41 +30,56 @@
         preload: function(entityID){
             _this.entityID = entityID;
             var properties = Entities.getEntityProperties(_this.entityID, ["position", "dimensions"]);
-            _this.homePos = properties.position;
-            var newY = (properties.dimensions.y * HALF) + _this.homePos.y;
-            topCenter = {x:_this.homePos.x, y:newY, z: _this.homePos.z};
-            soundEdge = SoundCache.getSound(Script.resolvePath("DrumKit/pearlkit-ridecrash.wav"));
-            soundBell = SoundCache.getSound(Script.resolvePath("DrumKit/pearlkit-ridebell.wav"));
-            soundMid = SoundCache.getSound(Script.resolvePath("DrumKit/pearlkit-ride1.wav"));
+            var newY = (properties.dimensions.y * 0.5) + properties.position.y;
+            topCenter = {x: properties.position.x, y:newY, z: properties.position.z};
         },
-        collisionWithEntity: function(thisEntity, otherEntity, collision) {
-            if (collision.type === 0) {
-                if (Vec3.distance(collision.contactPoint, topCenter) < 0.1) {
-                    this.playSound(soundBell);
-                } else if (Vec3.distance(collision.contactPoint, topCenter) < 0.2) {
-                    this.playSound(soundMid);
-                } else {
-                    this.playSound(soundEdge);
-                }
 
-            }
-        },
-        playSound: function(specificSound) {
-            _this.injector = Audio.playSound(_this.sound, {position: _this.homePos, volume: AUDIO_VOLUME_LEVEL});
-            if (specificSound.downloaded && !playing) {
-                Audio.playSound(specificSound, {
-                    position: _this.homePos,
-                    volume: AUDIO_VOLUME_LEVEL
-                });
+        // In HMD, listen for collisions and then calculate the distance between the point of collision and the top 
+        // center of the cymbal. If the collision occurred less than 0,1M from the center, play a bell sound. If it 
+        // occurred between 0.1M and 0.2M, play a bow sound. If the collision occurred more than 0.2M from the center, 
+        // play the crash sound. Collision type 0 is the start of a collision, whereas 1 would be the continuation 
+        // and 2 would be the end. The use of the variable "playing" with a timeout prevents the cymbal from being 
+        // played too quickly in succession.
+        collisionWithEntity: function(thisEntity, otherEntity, collision) {
+            if (collision.type === 0 && !playing) {
+                if (Vec3.distance(collision.contactPoint, topCenter) < 0.1) {
+                    _this.playSound(BELL_SOUND);
+                } else if (Vec3.distance(collision.contactPoint, topCenter) < 0.2) {
+                    _this.playSound(BOW_SOUND);
+                } else {
+                    _this.playSound(CRASH_SOUND);
+                }
                 playing = true;
                 Script.setTimeout(function() {
                     playing = false;
-                }, TIMEOUT_10_MS);
+                }, TIMEOUT_MS);
             }
         },
+
+        // In desktop, listen for mouse clicks and then check that it was the left mouse button before playing the cymbal.
+        // This will initiate the bow sound and a timeout as above.
         clickReleaseOnEntity: function(entityID, mouseEvent) {
-            if (mouseEvent.isLeftButton) {
-                this.playSound(soundBell);
+            if (mouseEvent.isLeftButton && !playing) {
+                _this.playSound(BOW_SOUND);
+                playing = true;
+                Script.setTimeout(function() {
+                    playing = false;
+                }, TIMEOUT_MS);
+            }
+        },
+
+        playSound: function(sound) {
+            if (sound.downloaded) {
+                injector = Audio.playSound(sound, {
+                    position: Entities.getEntityProperties(_this.entityID, ["position"]).position,
+                    volume: AUDIO_VOLUME_LEVEL
+                }); 
+            }
+        },
+
+        unload: function() {
+            if (injector) {
+                injector.stop();
             }
         }
     };
