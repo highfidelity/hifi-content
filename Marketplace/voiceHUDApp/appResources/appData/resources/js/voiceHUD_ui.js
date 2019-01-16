61574927e9adcf1465cbbc609870ba8c8d9faa13
Mark <33107309+MarkBrosche@users.noreply.github.com>
2019-01-15 16:27:07 -0800

    comment resolution, app now presists on domain change

--- Marketplace/voiceHUDApp/appResources/appData/resources/js/voiceHUD_ui.js
+++ Marketplace/voiceHUDApp/appResources/appData/resources/js/voiceHUD_ui.js
@@ -1,28 +1,33 @@
-var button = document.getElementById("toggle");
-var EVENT_BRIDGE_SETUP_DELAY = 100;
 
+// Send an event to the app script to 
+// toggle it on/off when the button is clicked
+var button = document.getElementById("toggle");
 function buttonClicked(){
     EventBridge.emitWebEvent(JSON.stringify({
         type: "TOGGLE_APP"
     }));
 }
-
 button.addEventListener("click", buttonClicked);
 
+
+// EventBridge message from App script.
 function onScriptEventReceived(data){
-// EventBridge message from HTML script.
     var data = JSON.parse(data);
     switch (data.type) {
         case "buttonStatus":
-            if ( data.value ) {
-                document.getElementById("toggle").value = "ON";
-            } else {
-                document.getElementById("toggle").value = "OFF";
-            }
-            break;
+        if (data.value) {
+            document.getElementById("toggle").value = "ON";
+        } else {
+            document.getElementById("toggle").value = "OFF";
+        }
+        break;
     }
 }
 
+
+// Set the text of the button to either On or Off 
+// when opening the tablet app, based on the app script status.
+var EVENT_BRIDGE_SETUP_DELAY = 100;
 function onLoad(){
     setTimeout(() => {
         EventBridge.scriptEventReceived.connect(onScriptEventReceived);    
