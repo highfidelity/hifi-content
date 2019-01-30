8ee19650b925f8dca70a6bfa91a348711df2a2d5
robin-k-wilson <robin.kath.wilson@gmail.com>
2019-01-30 15:21:26 -0800

    added folder structure and files for app

---
+++ marketplaceItems/avatarCustomization101/appResources/appData/resources/config.js
@@ -0,0 +1,19 @@
+//
+//  config.js 
+// 
+//  Variables shared by ui and javascript files.
+//
+//  Copyright 2019 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+
+/* global module */
+
+var CONFIG = {
+    // Variables shared by ui and javascript files
+};
+
+if (module) {
+    module.exports = CONFIG;
+}
