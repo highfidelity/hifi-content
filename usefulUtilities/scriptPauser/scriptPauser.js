5d396c5570521594593aec004538805d1a0a848c
Zach Fox <fox@highfidelity.io>
2019-02-26 17:53:41 -0800

    Improvements and CR

--- usefulUtilities/scriptPauser/scriptPauser.js
+++ usefulUtilities/scriptPauser/scriptPauser.js
@@ -22,81 +22,115 @@
     // The scripts in this list will be restarted once this entity script is unloaded.
     var pausedAppList = [];
 
-    var ScriptPauser = function() {};
+
+    // This function will put up a text banner notification informing the user that one
+    // of their scripts has been temporarily paused.
+    function notifyStoppedScript(scriptName) {
+        var text = "You are not allowed to run '" + scriptName +
+            "' in this place, so that script has been stopped. Go to a different place and it will restart.";
+        Window.displayAnnouncement(text);
+    }
+    
+
+    // When this interval expires, Script Pauser will check for blacklisted scripts and pause them.
+    var scriptPauserInterval = false;
+    var PAUSE_SCRIPTS_INTERVAL_MS = 10000;
+    function pauseScripts() {
+        var properties = Entities.getEntityProperties(_this.entityID, ["userData"]);
+        var userData;
+        scriptsToStopFuzzyMatch = [];
+        scriptsToStopExactMatch = [];
+
+        try {
+            userData = JSON.parse(properties.userData);
+        } catch (e) {
+            console.error("Error parsing userData: ", e);
+            return;
+        }
+
+        if (userData) {
+            if (userData.fuzzyScriptNames) {
+                if (Array.isArray(userData.fuzzyScriptNames)) {
+                    (userData.fuzzyScriptNames).forEach(function(scriptName) {
+                        if (scriptsToStopFuzzyMatch.indexOf(scriptName) === -1) {
+                            scriptsToStopFuzzyMatch.push(scriptName);
+                        }
+                    });
+                }
+            }
+
+            if (userData.exactScriptNames) {
+                if (Array.isArray(userData.exactScriptNames)) {
+                    (userData.exactScriptNames).forEach(function(scriptName) {
+                        if (scriptsToStopExactMatch.indexOf(scriptName) === -1) {
+                            scriptsToStopExactMatch.push(scriptName);
+                        }
+                    });
+                }
+            }
+        }
+                    
+        var currentlyRunningScripts = ScriptDiscoveryService.getRunning();
+
+        for (var i = 0; i < currentlyRunningScripts.length; i++) {
+            var currentScriptObject = currentlyRunningScripts[i];
+
+            for (var j = 0; j < scriptsToStopFuzzyMatch.length; j++) {
+                var currentFuzzyScriptName = scriptsToStopFuzzyMatch[j].toLowerCase();
+
+                if (((currentScriptObject.name).toLowerCase()).indexOf(currentFuzzyScriptName) > -1) {
+                    ScriptDiscoveryService.stopScript(currentScriptObject.url);
+                    notifyStoppedScript(currentScriptObject.name);
+
+                    if (pausedAppList.indexOf(currentScriptObject.url) === -1) {
+                        pausedAppList.push(currentScriptObject.url);
+                    }
+                }
+            }
+
+            for (var k = 0; k < scriptsToStopExactMatch.length; k++) {
+                var currentExactScriptName = scriptsToStopExactMatch[k];
+
+                if (currentScriptObject.name === currentExactScriptName) {
+                    ScriptDiscoveryService.stopScript(currentScriptObject.url);
+                    notifyStoppedScript(currentScriptObject.name);
+
+                    if (pausedAppList.indexOf(currentScriptObject.url) === -1) {
+                        pausedAppList.push(currentScriptObject.url);
+                    }
+                }
+            }
+        }
+    }
+
+    
+    var _this;
+    var ScriptPauser = function() {
+        _this = this;
+    };
+
 
     ScriptPauser.prototype = {
         preload: function (id) {
-            var properties = Entities.getEntityProperties(id, ["userData"]);
-            var userData;
-            scriptsToStopFuzzyMatch = [];
-            scriptsToStopExactMatch = [];
+            _this.entityID = id;
 
-            try {
-                userData = JSON.parse(properties.userData);
-            } catch (e) {
-                console.error("Error parsing userData: ", e);
-                return;
-            }
-
-            if (userData) {
-                if (userData.fuzzyScriptNames) {
-                    if (Array.isArray(userData.fuzzyScriptNames)) {
-                        (userData.fuzzyScriptNames).forEach(function (scriptName) {
-                            if (scriptsToStopFuzzyMatch.indexOf(scriptName) === -1) {
-                                scriptsToStopFuzzyMatch.push(scriptName);
-                            }
-                        });
-                    }
-                }
-
-                if (userData.exactScriptNames) {
-                    if (Array.isArray(userData.exactScriptNames)) {
-                        (userData.exactScriptNames).forEach(function (scriptName) {
-                            if (scriptsToStopExactMatch.indexOf(scriptName) === -1) {
-                                scriptsToStopExactMatch.push(scriptName);
-                            }
-                        });
-                    }
-                }
-            }
-                        
-            var currentlyRunningScripts = ScriptDiscoveryService.getRunning();
-
-            for (var i = 0; i < currentlyRunningScripts.length; i++) {
-                var currentScriptObject = currentlyRunningScripts[i];
-
-                for (var j = 0; j < scriptsToStopFuzzyMatch.length; j++) {
-                    var currentFuzzyScriptName = scriptsToStopFuzzyMatch[j].toLowerCase();
-
-                    if (((currentScriptObject.name).toLowerCase()).indexOf(currentFuzzyScriptName) > -1) {
-                        ScriptDiscoveryService.stopScript(currentScriptObject.url);
-
-                        if (pausedAppList.indexOf(currentScriptObject.url) === -1) {
-                            pausedAppList.push(currentScriptObject.url);
-                        }
-                    }
-                }
-
-                for (var k = 0; k < scriptsToStopExactMatch.length; k++) {
-                    var currentExactScriptName = scriptsToStopExactMatch[k];
-
-                    if (currentScriptObject.name === currentExactScriptName) {
-                        ScriptDiscoveryService.stopScript(currentScriptObject.url);
-
-                        if (pausedAppList.indexOf(currentScriptObject.url) === -1) {
-                            pausedAppList.push(currentScriptObject.url);
-                        }
-                    }
-                }
-            }
+            scriptPauserInterval = Script.setInterval(pauseScripts, PAUSE_SCRIPTS_INTERVAL_MS);
+            pauseScripts();     
         },
 
+
         unload: function() {
+            if (scriptPauserInterval) {
+                Script.clearInterval(scriptPauserInterval);
+                scriptPauserInterval = false;
+            }
+
             pausedAppList.forEach(function(url) {
                 ScriptDiscoveryService.loadScript(url);
             });
         }
     };
+
 
     return new ScriptPauser();
 });
