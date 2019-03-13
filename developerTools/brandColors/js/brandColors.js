a4f621136c8fcb16075a50cf2ee91ec4aebf62b3
milad <milad@miladnazeri.com>
2019-03-12 18:08:14 -0700

    fixed a color and added js counterpart

---
+++ developerTools/brandColors/js/brandColors.js
@@ -0,0 +1,27 @@
+/*
+
+    brandColors.js
+    Created by Milad Nazeri on 2019-03-12
+    Copyright 2019 High Fidelity, Inc.
+
+    Distributed under the Apache License, Version 2.0.
+    See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+
+    brand colors if needed in applications
+
+*/
+
+
+module.exports = {
+    HIFI_RED: "#ff001a",
+    HIFI_MAGENTA: "#ff42a7",
+    HIFI_NEUTRAL1: "#f1f3ee",
+    HIFI_NEUTRAL2: "#d8e1d9",
+    HIFI_NEUTRAL3: "#b7c8b9",
+    HIFI_NEUTRAL4: "#7e8c81",
+    HIFI_BLACK: "#000000",
+    HIFI_BLUE: "#172983",
+    HIFI_CYAN: "#009ee0",
+    HIFI_GREEN: "#009036",
+    HIFI_YELLOW: "#ffed00"
+};
