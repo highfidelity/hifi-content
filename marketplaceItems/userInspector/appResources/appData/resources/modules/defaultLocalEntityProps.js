6edbd947f96705a69b07a83b4c88426306442a29
milad <milad@miladnazeri.com>
2019-04-01 15:34:03 -0700

    updated for culling and colors

--- marketplaceItems/userInspector/appResources/appData/resources/modules/defaultLocalEntityProps.js
+++ marketplaceItems/userInspector/appResources/appData/resources/modules/defaultLocalEntityProps.js
@@ -15,14 +15,14 @@ var localEntityProps = {
     type: "Text",
     lineHeight: 0.1,
     // This will have to be changed in 81 until we bring unlit property to local text entities
-    textColor: "#EEEEEE",
+    textColor: "#ffffff",
     textAlpha: 1.0,
-    backgroundColor: "#2D2D2D",
+    backgroundColor: "#2d2d2d",
     backgroundAlpha: 1,
     billboardMode: "full",
-    lifetime: 5
+    lifetime: 20,
     // The following will be added back in 81
-    // renderLayer: "front",
+    renderLayer: "front"
 };
 
 module.exports = localEntityProps;
