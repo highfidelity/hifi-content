306f7e53eb22ee44de70bab7aad06e7e6332e253
Zach Fox <fox@highfidelity.io>
2019-04-04 10:54:23 -0700

    Custom status

--- usefulUtilities/officeMat/entityScripts/zones/squareZone.js
+++ usefulUtilities/officeMat/entityScripts/zones/squareZone.js
@@ -71,11 +71,32 @@
             queryParamString += "&displayName=" + MyAvatar.displayName;
             queryParamString += "&status=";
 
-            var isAvailable = false;
-            if (_this.squareType === "green") {
-                isAvailable = true;
+            var statusText = "busy";
+
+            var parentID = Entities.getEntityProperties(entityID, ["parentID"]).parentID;
+            var childrenIDs = Entities.getChildrenIDs(parentID);
+            var customStatusTextEntity = false;
+            for (var i = 0; i < childrenIDs.length; i++) {
+                if (Entities.getEntityProperties(childrenIDs[i], ["type"]).type === "Text") {
+                    customStatusTextEntity = childrenIDs[i];
+                    statusText = Entities.getEntityProperties(customStatusTextEntity, ["text"]).text;
+                    break;
+                }
             }
-            queryParamString += isAvailable ? "available" : "busy";
+
+            if (!customStatusTextEntity) {
+                if (_this.squareType === "green") {
+                    statusText = "available";
+                } else if (_this.squareType === "yellow") {
+                    statusText = "busy";
+                } else if (_this.squareType === "red") {
+                    statusText = "busy";
+                } else if (_this.squareType === "grey") {
+                    statusText = "busy";
+                }
+            }
+
+            queryParamString += statusText;
 
             request({
                 uri: REQUEST_URL + "?" + queryParamString
