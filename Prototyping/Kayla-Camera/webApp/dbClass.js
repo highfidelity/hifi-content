bd3c260524a63c1ce25f592f5f33f8112d6b62e0
milad <milad@miladnazeri.com>
2019-04-05 15:09:39 -0700

    remove logs from db class

--- Prototyping/Kayla-Camera/webApp/dbClass.js
+++ Prototyping/Kayla-Camera/webApp/dbClass.js
@@ -4,11 +4,9 @@ const mysql = require('mysql');
 
 class Database {
     constructor(dbConfig) {
-        console.log("dbConfig in constructor:", dbConfig)
         this.connection = mysql.createConnection(dbConfig);
     }
     query(sql) {
-        console.log("here in query", sql)
         return new Promise( (resolve, reject) => {
             this.connection.query(sql, (err,rows) => {
                 if (err){
@@ -31,7 +29,6 @@ class Database {
 }
 
 Database.execute = (config, callback) => {
-    console.log("in execute", JSON.stringify(config))
     const database = new Database(config);
     return callback(database).then(
         result => database.close().then( () => result ),
