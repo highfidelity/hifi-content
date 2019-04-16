c289e57c2c954979871d5b01464b43c45213ef16
robin-k-wilson <robin.kath.wilson@gmail.com>
2019-04-16 15:38:45 -0700

    added readme and console logs for signals

--- developerTools/scriptReferences/entityClientScript/entityClientScript.js
+++ developerTools/scriptReferences/entityClientScript/entityClientScript.js
@@ -4,26 +4,20 @@
 // Adapted from script https://github.com/robin-k-wilson/hifi-dev-utils/blob/master/client_entity_all_events.js
 // Updated by Robin Wilson 4/15/2019
 //
-// Generic client entity methods and signals that are triggered by mouse or controller interactions
-// Use as a template for your client entity scripts
-//
-// Client entity scripts are run on a interface instance. Meaning each avatar connected
-// to a domain have one instance of this script running locally and one client controls
-// an entity at one time called "ownership over that entity".
-// Best for physics changes.
-//
 // Copyright 2019 High Fidelity, Inc.
 //
 // Distributed under the Apache License, Version 2.0.
 // See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 
 (function() {
-    var I_AM_STATIC_VARIABLE = 200;
-    var _this = null;
-    var _entityID = null;
+    // example static variable
+    var I_AM_STATIC_VARIABLE = 200; 
+    var that = null;
 
     function EntityClientScript() {
-        _this = this;
+        that = this;
+        // dynamic variables for each instance of the script
+        this.entityID;
     }
 
     EntityClientScript.prototype = {
@@ -31,7 +25,7 @@
         preload: function(entityID) {
             // runs when this script is refreshed or a
             // client connects to a domain where this entity is present
-            _entityID = entityID;
+            that.entityID = entityID;
         },
 
         unload: function() {
@@ -51,65 +45,67 @@
         // params are: entityID, event PointerEvent (https://apidocs.highfidelity.com/global.html#PointerEvent)
         // https://apidocs.highfidelity.com/Entities.html#.clickDownOnEntity
         clickReleaseOnEntity: function(entityID, pointerEvent) {
-            if (pointerEvent.isPrimaryButton) {} // will only work on left mouse click release event (see PointerEvent)
+            if (pointerEvent.isPrimaryButton) {
+                console.log("EntityClientScript: clickReleaseOnEntity");
+            } // will only work on left mouse click release event (see PointerEvent)
         },
-        clickDownOnEntity: function(entityID, pointerEvent) {},
-        holdingClickOnEntity: function(entityID, pointerEvent) {},
+        clickDownOnEntity: function(entityID, pointerEvent) {console.log("EntityClientScript: clickDownOnEntity");},
+        holdingClickOnEntity: function(entityID, pointerEvent) {console.log("EntityClientScript: holdingClickOnEntity");},
         
-        mouseMoveOnEntity: function(entityID, pointerEvent) {},
-        mousePressOnEntity: function(entityID, event) {},
-        mouseReleaseOnEntity: function(entityID, event) {},
-        mouseDoublePressOffEntity: function(pointerEvent) {},
+        mouseMoveOnEntity: function(entityID, pointerEvent) {console.log("EntityClientScript: mouseMoveOnEntity");},
+        mousePressOnEntity: function(entityID, event) {console.log("EntityClientScript: mousePressOnEntity");},
+        mouseReleaseOnEntity: function(entityID, event) {console.log("EntityClientScript: mouseReleaseOnEntity");},
+        mouseDoublePressOffEntity: function(pointerEvent) {console.log("EntityClientScript: mouseDoublePressOffEntity");},
 
-        hoverEnterEntity: function(entityID, pointerEvent) {},
-        hoverLeaveEntity: function(entityID, pointerEvent) {},
-        hoverOverEntity: function(entityID, pointerEvent) {},
+        hoverEnterEntity: function(entityID, pointerEvent) {console.log("EntityClientScript: hoverEnterEntity");},
+        hoverLeaveEntity: function(entityID, pointerEvent) {console.log("EntityClientScript: hoverLeaveEntity");},
+        hoverOverEntity: function(entityID, pointerEvent) {console.log("EntityClientScript: hoverOverEntity");},
 
-        // triggered when a script in a Web entity's web page script sends and event over hte script's eventbridge
-        webEventReceived: function(entityID, message) {}, // https://apidocs.highfidelity.com/Entities.html#.webEventReceived
+        // triggered when a script in a Web entity's web page script sends and event over the script's eventbridge
+        webEventReceived: function(entityID, message) {console.log("EntityClientScript: webEventReceived");}, // https://apidocs.highfidelity.com/Entities.html#.webEventReceived
 
         // COLLISION METHODS
         // Avatar collision with this entity
-        leaveEntity: function(userID) {},
-        enterEntity: function(userID) {}, // https://apidocs.highfidelity.com/Entities.html#.enterEntity
+        leaveEntity: function(userID) {console.log("EntityClientScript: leaveEntity");},
+        enterEntity: function(userID) {console.log("EntityClientScript: enterEntity");}, // https://apidocs.highfidelity.com/Entities.html#.enterEntity
 
         // Entity (idB) collision with this entity (idA)
         // Collision properties https://apidocs.highfidelity.com/global.html#Collision
-        collisionWithEntity: function(idA, idB, collision) {}, // https://apidocs.highfidelity.com/Entities.html#.collisionWithEntity
+        collisionWithEntity: function(idA, idB, collision) {console.log("EntityClientScript: collisionWithEntity");}, // https://apidocs.highfidelity.com/Entities.html#.collisionWithEntity
 
         // DELETE
         // triggered when entity is deleted
-        deletingEntity: function(entityID) {},
+        deletingEntity: function(entityID) {console.log("EntityClientScript: deletingEntity");},
 
         // CONTROLLER METHODS
         // params are: entityID, string "<"left" || "right">,<MyAvatar.UUID>" ex "left,userID"
         // https://apidocs.highfidelity.com/Controller.html
 
         // Far Trigger Entity methods
-        startFarTrigger: function(entityID, handUserID) {},
-        continueFarTrigger: function(entityID, handUserID) {},
-        stopFarTrigger: function(entityID, handUserID) {},
+        startFarTrigger: function(entityID, handUserID) {console.log("EntityClientScript: startFarTrigger");},
+        continueFarTrigger: function(entityID, handUserID) {console.log("EntityClientScript: continueFarTrigger");},
+        stopFarTrigger: function(entityID, handUserID) {console.log("EntityClientScript: stopFarTrigger");},
 
         // Near trigger Entity methods
-        startNearTrigger: function(entityID, handUserID) {},
-        continueNearTrigger: function(entityID, handUserID) {},
-        stopNearTrigger: function(entityID, handUserID) {},
+        startNearTrigger: function(entityID, handUserID) {console.log("EntityClientScript: startNearTrigger");},
+        continueNearTrigger: function(entityID, handUserID) {console.log("EntityClientScript: continueNearTrigger");},
+        stopNearTrigger: function(entityID, handUserID) {console.log("EntityClientScript: stopNearTrigger");},
 
         // distanceGrab
-        startDistanceGrab: function(entityID, handUserID) {},
-        continueDistanceGrab: function(entityID, handUserID) {},
+        startDistanceGrab: function(entityID, handUserID) {console.log("EntityClientScript: startDistanceGrab");},
+        continueDistanceGrab: function(entityID, handUserID) {console.log("EntityClientScript: continueDistanceGrab");},
 
         // distanceGrab
-        startNearGrab: function(entityID, handUserID) {},
-        continueNearGrab: function(entityID, handUserID) {},
+        startNearGrab: function(entityID, handUserID) {console.log("EntityClientScript: startNearGrab");},
+        continueNearGrab: function(entityID, handUserID) {console.log("EntityClientScript: continueNearGrab");},
 
         // releases the trigger
-        releaseGrab: function(entityID, handUserID) {},
+        releaseGrab: function(entityID, handUserID) {console.log("EntityClientScript: releaseGrab");},
 
         // Equip
-        startEquip: function(entityID, handUserID) {},
-        continueEquip: function(entityID, handUserID) {},
-        releaseEquip: function(entityID, handUserID) {},
+        startEquip: function(entityID, handUserID) {console.log("EntityClientScript: startEquip");},
+        continueEquip: function(entityID, handUserID) {console.log("EntityClientScript: continueEquip");},
+        releaseEquip: function(entityID, handUserID) {console.log("EntityClientScript: releaseEquip");},
     }
 
     return new EntityClientScript();
