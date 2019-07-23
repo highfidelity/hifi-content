45b2e3eae112c456f4d407d98218067537c21fa5
Zach Fox <fox@highfidelity.io>
2019-07-23 09:37:55 -0700

    Sound Emitter v1.2

--- usefulUtilities/soundEmitter/soundEmitter.js
+++ usefulUtilities/soundEmitter/soundEmitter.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-magic-numbers */
 //
-// soundEmitterServer.js
+// soundEmitter.js
 // 
 // Created by Zach Fox on 2019-07-05
 // Copyright High Fidelity 2019
@@ -24,7 +24,7 @@
     };
 
 
-    var SoundEmitterServer = function() {
+    var SoundEmitter = function() {
         that = this;
         that.entityID = false;
         that.soundObjectURL = false;
@@ -36,7 +36,7 @@
     };
 
 
-    SoundEmitterServer.prototype = {
+    SoundEmitter.prototype = {
         preload: function(entityID) {
             that.entityID = entityID;
 
@@ -111,6 +111,13 @@
         },
 
 
+        positionChanged: function(newPos) {
+            return (newPos.x !== that.audioInjectorOptions.position.x ||
+                newPos.y !== that.audioInjectorOptions.position.y ||
+                newPos.z !== that.audioInjectorOptions.position.z);
+        },
+
+
         updateSoundEmitter: function() {
             var properties = Entities.getEntityProperties(that.entityID, ["userData", "position"]);
 
@@ -124,13 +131,7 @@
 
             var optionsChanged = false;
             var shouldRestartPlayback = false;
-
-            if (properties.position.x !== that.audioInjectorOptions.position.x ||
-                properties.position.y !== that.audioInjectorOptions.position.y ||
-                properties.position.z !== that.audioInjectorOptions.position.z) {
-                optionsChanged = true;
-                that.audioInjectorOptions["position"] = properties.position;
-            }
+            var newPosition = properties.position;
 
             if (userData) {
                 if (userData.soundURL && userData.soundURL.length > 0 && userData.soundURL !== that.soundObjectURL) {
@@ -158,10 +159,20 @@
                     optionsChanged = true;
                     that.audioInjectorOptions.localOnly = userData.localOnly;
                 }
+
+                if (typeof(userData.positionOverride) !== "undefined" && !isNaN(userData.positionOverride.x) &&
+                    !isNaN(userData.positionOverride.y) && !isNaN(userData.positionOverride.z)) {
+                    newPosition = userData.positionOverride;
+                }
             } else {
                 console.log("Please specify this entity's `userData`! See README.md for instructions.");
                 that.clearCurrentSoundData();
                 return;
+            }
+
+            if (that.positionChanged(newPosition)) {
+                optionsChanged = true;
+                that.audioInjectorOptions["position"] = newPosition;
             }
 
             if (!that.audioInjector || shouldRestartPlayback) {
@@ -177,5 +188,5 @@
     };
 
     
-    return new SoundEmitterServer();
+    return new SoundEmitter();
 });
